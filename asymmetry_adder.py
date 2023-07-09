187d7b9e0b59feea1b1ccecbcba7236b6d5cb4a6
RNTG <rntg@dhcp-10-249-173-74.eduroam.wireless.private.cam.ac.uk>
2023-07-09 01:18:55 +0100

    script for calculating and adding asymmetry term to the data. WIP.

---
+++ asymmetry_adder.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+df = pd.read_csv("ENDFBVIII_zeroed_LDP_XS.csv")
+
+print(df.shape)
+
+# insert at 67, 68, 69
+
+nucs = []
+
+for i, j, in zip(df['A'], df['Z']):  # i is A, j is Z
+    if [j, i] in nucs:
+        continue
+    else:
+        nucs.append([j, i])  # format is [Z, A]
+
+print(nucs)
+
+
+def asymmetry_term(N, Z, A):
+
+    s_target = (N - Z)/A
+
+    compound_N = N+1
+    compound_A = A+1
+    s_compound = (compound_N - Z) / compound_A
+
+    daughter_N = N-1
+    daughter_A = A-1
+    s_daughter = (daughter_N - Z)/daughter_A
+
+    return s_target, s_compound, s_daughter
+
+s, s_c, s_d = asymmetry_term(N=df['N'], Z=df['Z'], A=df['A'])
+
+# for n, z, a in zip(df['N'], df['Z'], df['A']):
+#     pass
