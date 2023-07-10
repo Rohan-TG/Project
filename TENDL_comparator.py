31c6b7299549cf2c7b0d677bb28120cbeae173b8
RNTG <rntg@dhcp-10-248-156-84.eduroam.wireless.private.cam.ac.uk>
2023-07-10 20:32:34 +0100

    WIP - train, and test by comparing to TENDL. Intended for exotic nuclides

---
+++ TENDL_comparator.py
@@ -0,0 +1,144 @@
+import warnings
+from numba.core.errors import NumbaDeprecationWarning
+warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import xgboost as xg
+import time
+import shap
+from sklearn.metrics import mean_squared_error, r2_score
+import periodictable
+from matrix_functions import make_test, make_train, anomaly_remover, range_setter
+
+
+
+df_test = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
+df = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
+
+df_test = df_test[df_test.Z != 11]
+df = df[df.Z != 11]
+
+TENDL_DATAFRAME = pd.read_csv()
+
+# df_test = df_test[df_test.MT == 16] # extract (n,2n) only
+df_test.index = range(len(df_test)) # re-label indices
+df.index = range(len(df))
+
+
+df_test = anomaly_remover(dfa = df_test)
+
+# def custom_loss(y_pred, y_true):
+# 	num = y_pred + 1
+# 	den = y_true + 1
+#
+# 	ln = np.log(y_pred + 1)
+#
+# 	grad = (num/den) - ln
+# 	hess = np.repeat(2, y_true.shape[0])
+#
+# 	return grad, hess
+
+al = range_setter(la=30, ua=215, df=df)
+
+magic_numbers = [2, 8, 20, 28, 50, 82, 126]
+
+doubly_magic = []
+n_magic = []
+p_magic = []
+all_magic = []
+
+for nuc in al:
+	if nuc[0] in magic_numbers and (nuc[1] - nuc[0]) in magic_numbers:
+		# print(f"Double: {nuc} - {periodictable.elements[nuc[0]]}-{nuc[1]}")
+		doubly_magic.append(nuc)
+		all_magic.append(nuc)
+	elif nuc[0] in magic_numbers and (nuc[1] - nuc[0]) not in magic_numbers:
+		# print(f"Protonic: {nuc} - {periodictable.elements[nuc[0]]}-{nuc[1]}")
+		p_magic.append(nuc)
+		all_magic.append(nuc)
+	elif nuc[0] not in magic_numbers and (nuc[1] - nuc[0]) in magic_numbers:
+		# print(f"Neutronic: {nuc} - {periodictable.elements[nuc[0]]}-{nuc[1]}")
+		n_magic.append(nuc)
+		all_magic.append(nuc)
+
+
+validation_nuclides = []
+validation_set_size = 50 # number of nuclides hidden from training
+
+while len(validation_nuclides) < validation_set_size:
+	choice = random.choice(al) # randomly select nuclide from list of all nuclides
+	if choice not in validation_nuclides:
+		validation_nuclides.append(choice)
+print("Test nuclide selection complete")
+
+
+
+if __name__ == "__main__":
+
+	X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=30, ua=215) # make training matrix
+
+	X_test, y_test = make_test(validation_nuclides, df=df_test)
+
+	# X_train must be in the shape (n_samples, n_features)
+	# and y_train must be in the shape (n_samples) of the target
+
+	print("Data prep done")
+
+
+	model = xg.XGBRegressor(n_estimators=900,
+							learning_rate=0.015,
+							max_depth=8,
+							subsample=0.18236,
+							max_leaves=0,
+							seed=42,)
+
+
+	time1 = time.time()
+	model.fit(X_train, y_train)
+
+	print("Training complete")
+
+	predictions = model.predict(X_test) # XS predictions
+
+	# shap_val_gpu = model.predict(X_test, pred_interactions=True)
+
+	# Form arrays for plots below
+	XS_plotmatrix = []
+	E_plotmatrix = []
+	P_plotmatrix = []
+	for nuclide in validation_nuclides:
+		dummy_test_XS = []
+		dummy_test_E = []
+		dummy_predictions = []
+		for i, row in enumerate(X_test):
+			if [row[0], row[1]] == nuclide:
+				dummy_test_XS.append(y_test[i])
+				dummy_test_E.append(row[4]) # Energy values are in 5th row
+				dummy_predictions.append(predictions[i])
+
+		XS_plotmatrix.append(dummy_test_XS)
+		E_plotmatrix.append(dummy_test_E)
+		P_plotmatrix.append(dummy_predictions)
+
+	# plot predictions against data
+	# note: python lists allow elements to be lists of varying lengths. This would not work using numpy arrays; the for
+	# loop below loops through the lists ..._plotmatrix, where each element is a list corresponding to nuclide nuc[i].
+	for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
+		nuc = validation_nuclides[i] # validation nuclide
+		# plt.plot(erg, pred_xs, label='predictions')
+		# plt.plot(erg, true_xs, label='data')
+		# plt.title(f"(n,2n) XS for {periodictable.elements[nuc[0]]}-{nuc[1]:0.0f}")
+		# plt.legend()
+		# plt.grid()
+		# plt.ylabel('XS / b')
+		# plt.xlabel('Energy / MeV')
+		# plt.show()
+
+		r2 = r2_score(true_xs, pred_xs) # R^2 score for this specific nuclide
+		print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
+		# time.sleep(1)
+
+	print(f"MSE: {mean_squared_error(y_test, predictions, squared=False)}") # MSE
+	print(f"R2: {r2_score(y_test, predictions)}") # Total R^2 for all predictions in this training campaign
+	print(f'completed in {time.time() - time1} s')
