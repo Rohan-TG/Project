5542e75dbddb04cf1c53132ac8c4b055a6a85a63
RNTG <rntg@dhcp-10-249-165-137.eduroam.wireless.private.cam.ac.uk>
2023-07-22 01:41:57 +0100

    cleaned

--- train_on_everything.py
+++ train_on_everything.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import periodictable
 import xgboost as xg
-from matrix_functions import make_test, make_train, range_setter
+from matrix_functions import make_test, make_train, range_setter, General_plotter
 from sklearn.metrics import r2_score, mean_squared_error
 import time
 
@@ -24,11 +24,20 @@ nucs = range_setter(df=all_libraries, la=30, ua=210)
 all_libraries.index = range(len(all_libraries))
 
 
-validation_nuclides = [[39,85], [39,86],[39,87],
-					   [39,88],[39,90],[39,91],
-					   [39,92],[39,93],[39,94],
-					   [39,95]]
-validation_set_size = 5 # number of nuclides hidden from training
+# validation_nuclides = [[39,85], [39,86],[39,87],
+# 					   [39,88],[39,90],[39,91],
+# 					   [39,92],[39,93],[39,94],
+# 					   [39,95]]
+
+# validation_nuclides = []
+
+validation_nuclides = [[40,85], [40,86], [40,87],
+					   [40,88], [40,89], [40,93],
+					   [40,94], [40,95], [40,96],
+					   [40,97], [40,98], [40,99],
+					   [40,100], [40,101], [40,102]]
+
+validation_set_size = 10 # number of nuclides hidden from training
 
 while len(validation_nuclides) < validation_set_size:
 	choice = random.choice(nucs) # randomly select nuclide from list of all nuclides
@@ -43,7 +52,8 @@ X_test, y_test = make_test(validation_nuclides, df=tendl21)
 print("Data prep done")
 
 model = xg.XGBRegressor(n_estimators=850,
-                        learning_rate=0.01987,
+                        # learning_rate=0.01987,
+                        learning_rate=0.01,
                         max_depth=11,
                         subsample=0.1314,
                         max_leaves=0,
@@ -84,8 +94,24 @@ for nuclide in validation_nuclides:
 
 for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
     nuc = validation_nuclides[i]  # validation nuclide
-    plt.plot(erg, pred_xs, label='predictions')
-    plt.plot(erg, true_xs, label='data')
+
+    endfberg, endfbxs = General_plotter(df=endfb8, nuclides=[nuc])
+    endfb_nucs = range_setter(df=endfb8, la=30, ua=210)
+    jendlerg, jendlxs = General_plotter(df=jendl5, nuclides=[nuc])
+    jendl_nucs = range_setter(df=jendl5, la=30, ua=210)
+    cendlerg, cendlxs = General_plotter(df=cendl32, nuclides=[nuc])
+    cendl_nucs= range_setter(df=cendl32, la=30, ua=210)
+    jefferg, jeffxs = General_plotter(df=jeff33, nuclides=[nuc])
+    jeff_nucs = range_setter(df=jeff33, la=30, ua=210)
+
+    plt.plot(erg, pred_xs, label='Predictions', color = 'red', linewidth=2)
+    plt.plot(erg, true_xs, label = "TENDL21", color='dimgrey', linewidth=2)
+    if nuc in jeff_nucs:
+        plt.plot(jefferg, jeffxs, '--', label='JEFF3.3', color='mediumvioletred')
+    if nuc in cendl_nucs:
+        plt.plot(cendlerg, cendlxs, '--', label='CENDL3.3', color='gold')
+    if nuc in jendl_nucs:
+        plt.plot(jendlerg, jendlxs, label='JENDL5', color='green')
     plt.title(f"(n,2n) XS for {periodictable.elements[nuc[0]]}-{nuc[1]:0.0f}")
     plt.legend()
     plt.grid()
@@ -95,7 +121,7 @@ for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_p
 
     r2 = r2_score(true_xs, pred_xs)  # R^2 score for this specific nuclide
     print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
-    time.sleep(1.2)
+    # time.sleep(1.2)
 
 print(f"MSE: {mean_squared_error(y_test, predictions, squared=False)}")  # MSE
 print(f"R2: {r2_score(y_test, predictions)}")  # Total R^2 for all predictions in this training campaign
