e250d1b77045f334617a5466813cf7e5eb69cca5
T G <rnt26@cam.ac.uk>
2023-07-23 17:58:41 +0100

    updated

--- Benchmarker_high_A.py
+++ Benchmarker_high_A.py
@@ -10,15 +10,10 @@ import time
 # import shap
 from sklearn.metrics import mean_squared_error, r2_score
 import periodictable
-from matrix_functions import anomaly_remover, make_train, make_test
-import tqdm
+from matrix_functions import anomaly_remover, make_train, make_test, range_setter
 
-
-# df = pd.read_csv('zeroed_1_xs_fund_feateng.csv') # new interpolated dataset, used for training only
-# df_test = pd.read_csv('zeroed_1_xs_fund_feateng.csv') # original dataset, used for validation
-
-df = pd.read_csv("ENDFBVIII_zeroed_LDP_XS.csv")
-df_test = pd.read_csv("ENDFBVIII_zeroed_LDP_XS.csv")  # dataframe as above, but with the new features from the Gilbert-Cameron model
+df = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
+df_test = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")  # dataframe as above, but with the new features from the Gilbert-Cameron model
 
 
 
@@ -31,91 +26,12 @@ df_test = anomaly_remover(dfa = df_test)
 # 	else:
 # 		al.append([j, i]) # format is [Z, A]
 
-def range_setter(la, ua):
 
-	nucs = []
 
-	for i, j, in zip(df['A'], df['Z']): # i is A, j is Z
-		if [j, i] in nucs or i > ua or i < la:
-			continue
-		else:
-			nucs.append([j, i]) # format is [Z, A]
-
-	return nucs
-
-al = range_setter(la=30, ua=50)
-
-# cat_magic = []
-#
-# magic_numbers = [2, 8, 20, 28, 50, 82, 126]
-#
-#
-# cat_magic_double_original = []
-# cat_magic_neutron_original = []
-# cat_magic_proton_original = []
-#
-# for z, n in zip(df_test['Z'], df_test['A']):
-# 	if z in magic_numbers and n in magic_numbers:
-# 		cat_magic_proton_original.append(1)
-# 		cat_magic_double_original.append(1)
-# 		cat_magic_neutron_original.append(1)
-# 	elif z in magic_numbers and n not in magic_numbers:
-# 		cat_magic_proton_original.append(1)
-# 		cat_magic_neutron_original.append(0)
-# 		cat_magic_double_original.append(0)
-# 	elif z not in magic_numbers and n in magic_numbers:
-# 		cat_magic_neutron_original.append(1)
-# 		cat_magic_double_original.append(0)
-# 		cat_magic_proton_original.append(0)
-# 	else:
-# 		cat_magic_proton_original.append(0)
-# 		cat_magic_double_original.append(0)
-# 		cat_magic_neutron_original.append(0)
-#
-# df_test.insert(78, 'cat_magic_proton', cat_magic_proton_original)
-# df_test.insert(79, 'cat_magic_neutron', cat_magic_neutron_original)
-# df_test.insert(80, 'cat_magic_double', cat_magic_double_original)
-#
-# df_test['cat_magic_proton'].astype('category')
-# df_test['cat_magic_neutron'].astype('category')
-# df_test['cat_magic_double'].astype("category")
-#
-#
-# cat_magic_proton = []
-# cat_magic_neutron = []
-# cat_magic_double = []
-#
-# for z, n in zip(df['Z'], df['N']):
-# 	if z in magic_numbers and n in magic_numbers:
-# 		cat_magic_double.append(1)
-# 		cat_magic_neutron.append(1)
-# 		cat_magic_proton.append(1)
-# 	elif z in magic_numbers and n not in magic_numbers:
-# 		cat_magic_proton.append(1)
-# 		cat_magic_neutron.append(0)
-# 		cat_magic_double.append(0)
-# 	elif z not in magic_numbers and n in magic_numbers:
-# 		cat_magic_neutron.append(1)
-# 		cat_magic_proton.append(0)
-# 		cat_magic_double.append(0)
-# 	else:
-# 		cat_magic_proton.append(0)
-# 		cat_magic_double.append(0)
-# 		cat_magic_neutron.append(0)
-#
-#
-# df.insert(78, 'cat_magic_proton', cat_magic_proton)
-# df.insert(79, 'cat_magic_neutron', cat_magic_neutron)
-# df.insert(80, 'cat_magic_double', cat_magic_double)
-#
-# df['cat_magic_proton'].astype('category')
-# df['cat_magic_neutron'].astype('category')
-# df['cat_magic_double'].astype("category")
-
+al = range_setter(la=0, ua=270)
 
 
 nuclides_used = []
-
 nuclide_mse = []
 nuclide_r2 = []
 
@@ -136,7 +52,7 @@ if __name__ == "__main__":
 
 		validation_nuclides = []  # list of nuclides used for validation
 		# test_nuclides = []
-		validation_set_size = 10  # number of nuclides hidden from training
+		validation_set_size = 20  # number of nuclides hidden from training
 
 		while len(validation_nuclides) < validation_set_size:
 
@@ -153,7 +69,7 @@ if __name__ == "__main__":
 		# print(f"Epoch {len(al) // len(nuclides_used) + 1}/")
 
 
-		X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=0, ua=50) # make training matrix
+		X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=0, ua=270) # make training matrix
 
 		X_test, y_test = make_test(validation_nuclides, df=df_test)
 
@@ -232,133 +148,6 @@ if __name__ == "__main__":
 		# time.sleep(10)
 
 
-		# explainer = shap.Explainer(model.predict, X_train,
-		# 						   feature_names= ['Z', 'A', 'S2n', 'S2p', 'E', 'Sp', 'Sn',
-		# 										   'BEA', 'P', 'Snc', 'g-def', 'N',
-		# 										   'b-def',
-		# 										   'Sn_da',
-		# 										   'Sp_d',
-		# 										   'S2n_d',
-		# 										   'Radius',
-		# 										   'n_g_erg',
-		# 										   'n_c_erg',
-		# 										   # 'xsmax',
-		# 										   'n_rms_r',
-		# 										   # 'oct_def',
-		# 										   'D_c',
-		# 										   'BEA_d',
-		# 										   'BEA_c',
-		# 										   'Pair_d',
-		# 										   # 'Par_d',
-		# 										   'S2n_c',
-		# 										   'S2p_c',
-		# 										   'ME',
-		# 										   # 'Z_even',
-		# 										   # 'A_even',
-		# 										   # 'N_even',
-		# 										   'Shell',
-		# 										   # 'Par',
-		# 										   'Spin',
-		# 										   'Decay',
-		# 										   'Deform',
-		# 										   'p_g_e',
-		# 										   'p_c_e',
-		# 										   'p_rms_r',
-		# 										   'rms_r',
-		# 										   'Sp_c',
-		# 										   'S_n_c',
-		# 										   'Shell_c',
-		# 										   # 'S2p-d',
-		# 										   'Shell-d',
-		# 										   'Spin-c',
-		# 										   'Rad-c',
-		# 										   'Def-c',
-		# 										   'ME-c',
-		# 										   'BEA-A-c',
-		# 										   'Decay-d',
-		# 										   'ME-d',
-		# 										   'Rad-d',
-		# 										   'Pair-c',
-		# 										   # 'Par-c',
-		# 										   'BEA-A-d',
-		# 										   # 'Spin-d',
-		# 										   'Def-d',
-		# 										   'mag_p',
-		# 										   'mag_n',
-		# 										   'mag_d',
-		# 										   ]) # SHAP feature importance analysis
-		# shap_values = explainer(X_test)
-
-		# name features for FIA
-		# model.get_booster().feature_names = ['Z', 'A', 'S2n', 'S2p', 'E', 'Sp', 'Sn',
-		# 									 'BEA',
-		# 									 # 'P',
-		# 									 'Snc', 'g-def',
-		# 									 'N',
-		# 									 'b-def',
-		# 									 'Sn da',
-		# 									 'Sp d',
-		# 									 'S2n d',
-		# 									 'Radius',
-		# 									 'n_g_erg',
-		# 									 'n_c_erg',
-		# 									 # 'xsmax',
-		# 									 'n_rms_r',
-		# 									 # 'oct_def',
-		# 									 'D_c',
-		# 									 'BEA_d',
-		# 									 'BEA_c',
-		# 									 # 'Pair_d',
-		# 									 # 'Par_d',
-		# 									 'S2n_c',
-		# 									 'S2p_c',
-		# 									 'ME',
-		# 									 # 'Z_even',
-		# 									 # 'A_even',
-		# 									 # 'N_even',
-		# 									 'Shell',
-		# 									 'Parity',
-		# 									 'Spin',
-		# 									 'Decay',
-		# 									 'Deform',
-		# 									 'p_g_e',
-		# 									 'p_c_e',
-		# 									 'p_rms_r',
-		# 									 'rms_r',
-		# 									 'Sp_c',
-		# 									 # 'Sn_c',
-		# 									 'Shell_c',
-		# 									 # 'S2p-d',
-		# 									 # 'Shell-d',
-		# 									 'Spin-c',
-		# 									 'Rad-c',
-		# 									 'Def-c',
-		# 									 'ME-c',
-		# 									 'BEA-A-c',
-		# 									 'Decay-d',
-		# 									 'ME-d',
-		# 									 # 'Rad-d',
-		# 									 # 'Pair-c',
-		# 									 # 'Par-c',
-		# 									 'BEA-A-d',
-		# 									 # 'Spin-d',
-		# 									 'Def-d',
-		# 									 # 'mag_p',
-		# 									 # 'mag-n',
-		# 									 # 'mag-d',
-		# 									 'Nlow',
-		# 									 'Ulow',
-		# 									 'Ntop',
-		# 									 'Utop',
-		# 									 'ainf',
-		# 									 ]
-		#
-		# plt.figure(figsize=(10,12))
-		# xg.plot_importance(model, ax=plt.gca(), importance_type='total_gain', max_num_features=60) # metric is total gain
-		# plt.show()
-
-		# shap.plots.bar(shap_values, max_display = 70) # display SHAP results
-		# shap.plots.waterfall(shap_values[0], max_display=70)
 
 print()
 
