b61a96756848bf0c2f636bb9c40400b7638f604f
RNTG <rntg@dhcp-10-249-167-1.eduroam.wireless.private.cam.ac.uk>
2023-07-23 23:49:05 +0100

    cleaning

--- train_on_everything.py
+++ train_on_everything.py
@@ -52,11 +52,11 @@ X_test, y_test = make_test(validation_nuclides, df=tendl21)
 print("Data prep done")
 
 model = xg.XGBRegressor(n_estimators=850,
-                        learning_rate=0.007,
-                        max_depth=11,
-                        subsample=0.1314,
-                        max_leaves=0,
-                        seed=42, )
+						learning_rate=0.007,
+						max_depth=11,
+						subsample=0.1314,
+						max_leaves=0,
+						seed=42, )
 
 time1= time.time()
 
@@ -67,10 +67,10 @@ print("Training complete")
 predictions = model.predict(X_test)  # XS predictions
 predictions_ReLU = []
 for pred in predictions:
-    if pred >= 0.001:
-        predictions_ReLU.append(pred)
-    else:
-        predictions_ReLU.append(0)
+	if pred >= 0.001:
+		predictions_ReLU.append(pred)
+	else:
+		predictions_ReLU.append(0)
 
 predictions = predictions_ReLU
 
@@ -78,51 +78,51 @@ XS_plotmatrix = []
 E_plotmatrix = []
 P_plotmatrix = []
 for nuclide in validation_nuclides:
-    dummy_test_XS = []
-    dummy_test_E = []
-    dummy_predictions = []
-    for i, row in enumerate(X_test):
-        if [row[0], row[1]] == nuclide:
-            dummy_test_XS.append(y_test[i])
-            dummy_test_E.append(row[4])  # Energy values are in 5th row
-            dummy_predictions.append(predictions[i])
+	dummy_test_XS = []
+	dummy_test_E = []
+	dummy_predictions = []
+	for i, row in enumerate(X_test):
+		if [row[0], row[1]] == nuclide:
+			dummy_test_XS.append(y_test[i])
+			dummy_test_E.append(row[4])  # Energy values are in 5th row
+			dummy_predictions.append(predictions[i])
 
-    XS_plotmatrix.append(dummy_test_XS)
-    E_plotmatrix.append(dummy_test_E)
-    P_plotmatrix.append(dummy_predictions)
+	XS_plotmatrix.append(dummy_test_XS)
+	E_plotmatrix.append(dummy_test_E)
+	P_plotmatrix.append(dummy_predictions)
 
 for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
-    nuc = validation_nuclides[i]  # validation nuclide
+	nuc = validation_nuclides[i]  # validation nuclide
 
-    endfberg, endfbxs = General_plotter(df=endfb8, nuclides=[nuc])
-    endfb_nucs = range_setter(df=endfb8, la=30, ua=210)
-    jendlerg, jendlxs = General_plotter(df=jendl5, nuclides=[nuc])
-    jendl_nucs = range_setter(df=jendl5, la=30, ua=210)
-    cendlerg, cendlxs = General_plotter(df=cendl32, nuclides=[nuc])
-    cendl_nucs= range_setter(df=cendl32, la=30, ua=210)
-    jefferg, jeffxs = General_plotter(df=jeff33, nuclides=[nuc])
-    jeff_nucs = range_setter(df=jeff33, la=30, ua=210)
+	endfberg, endfbxs = General_plotter(df=endfb8, nuclides=[nuc])
+	endfb_nucs = range_setter(df=endfb8, la=30, ua=210)
+	jendlerg, jendlxs = General_plotter(df=jendl5, nuclides=[nuc])
+	jendl_nucs = range_setter(df=jendl5, la=30, ua=210)
+	cendlerg, cendlxs = General_plotter(df=cendl32, nuclides=[nuc])
+	cendl_nucs= range_setter(df=cendl32, la=30, ua=210)
+	jefferg, jeffxs = General_plotter(df=jeff33, nuclides=[nuc])
+	jeff_nucs = range_setter(df=jeff33, la=30, ua=210)
 
-    plt.plot(erg, pred_xs, label='Predictions', color = 'red', linewidth=2)
-    plt.plot(erg, true_xs, label = "TENDL21", color='dimgrey', linewidth=2)
-    if nuc in jeff_nucs:
-        plt.plot(jefferg, jeffxs, '--', label='JEFF3.3', color='mediumvioletred')
-    if nuc in cendl_nucs:
-        plt.plot(cendlerg, cendlxs, '--', label='CENDL3.3', color='gold')
-    if nuc in jendl_nucs:
-        plt.plot(jendlerg, jendlxs, label='JENDL5', color='green')
-    if nuc in endfb_nucs:
-        plt.plot(endfberg, endfbxs, label='ENDF/B-VIII')
-    plt.title(f"(n,2n) XS for {periodictable.elements[nuc[0]]}-{nuc[1]:0.0f}")
-    plt.legend()
-    plt.grid()
-    plt.ylabel('XS / b')
-    plt.xlabel('Energy / MeV')
-    plt.show()
+	plt.plot(erg, pred_xs, label='Predictions', color = 'red', linewidth=2)
+	plt.plot(erg, true_xs, label = "TENDL21", color='dimgrey', linewidth=2)
+	if nuc in jeff_nucs:
+		plt.plot(jefferg, jeffxs, '--', label='JEFF3.3', color='mediumvioletred')
+	if nuc in cendl_nucs:
+		plt.plot(cendlerg, cendlxs, '--', label='CENDL3.3', color='gold')
+	if nuc in jendl_nucs:
+		plt.plot(jendlerg, jendlxs, label='JENDL5', color='green')
+	if nuc in endfb_nucs:
+		plt.plot(endfberg, endfbxs, label='ENDF/B-VIII')
+	plt.title(f"(n,2n) XS for {periodictable.elements[nuc[0]]}-{nuc[1]:0.0f}")
+	plt.legend()
+	plt.grid()
+	plt.ylabel('XS / b')
+	plt.xlabel('Energy / MeV')
+	plt.show()
 
-    r2 = r2_score(true_xs, pred_xs)  # R^2 score for this specific nuclide
-    print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
-    time.sleep(2)
+	r2 = r2_score(true_xs, pred_xs)  # R^2 score for this specific nuclide
+	print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
+	time.sleep(2)
 
 print(f"MSE: {mean_squared_error(y_test, predictions, squared=False)}")  # MSE
 print(f"R2: {r2_score(y_test, predictions)}")  # Total R^2 for all predictions in this training campaign
