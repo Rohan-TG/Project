94d4c1b33fa06d477652a361258ccbfd1de313d8
RNTG <rntg@dhcp-10-249-188-120.eduroam.wireless.private.cam.ac.uk>
2023-08-11 22:32:28 +0100

    changed learning rate

--- Exotic_predictions_ReLU.py
+++ Exotic_predictions_ReLU.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 	print("Data prep done")
 
 	model = xg.XGBRegressor(n_estimators=900,
-							learning_rate=0.01,
+							learning_rate=0.008,
 							max_depth=8,
 							subsample=0.18236,
 							max_leaves=0,
