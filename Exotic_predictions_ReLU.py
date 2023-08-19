1ea25e6666dcdfce74c4b3c253acf1a61e433085
RNTG <rntg@dhcp-10-249-177-198.eduroam.wireless.private.cam.ac.uk>
2023-08-19 21:05:54 +0100

    fixed up

--- Exotic_predictions_ReLU.py
+++ Exotic_predictions_ReLU.py
@@ -38,103 +38,106 @@ print("Data loaded")
 # 					   [40,99],[40,100], [40,101],
 # 					   [40,102]]
 
-validation_nuclides = [[49,99]]
 
-validation_set_size = 1 # number of nuclides hidden from training
+exotic_nuclides = []
+for x in TENDL_nuclides:
+	if x not in al:
+		exotic_nuclides.append(x)
+
+validation_nuclides = []
+
+validation_set_size = 1 #
 
 while len(validation_nuclides) < validation_set_size:
-	choice = random.choice(al) # randomly select nuclide from list of all nuclides
+	choice = random.choice(exotic_nuclides) # randomly select nuclide from list of all nuclides
 	if choice not in validation_nuclides:
 		validation_nuclides.append(choice)
 print("Test nuclide selection complete")
 
 
+X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=30, ua=215) # make training matrix
 
-if __name__ == "__main__":
+X_test, y_test = make_test(validation_nuclides, df=TENDL)
 
-	X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=30, ua=215) # make training matrix
+# X_train must be in the shape (n_samples, n_features)
+# and y_train must be in the shape (n_samples) of the target
 
-	X_test, y_test = make_test(validation_nuclides, df=TENDL)
+print("Data prep done")
 
-	# X_train must be in the shape (n_samples, n_features)
-	# and y_train must be in the shape (n_samples) of the target
+model = xg.XGBRegressor(n_estimators=900,
+						learning_rate=0.008,
+						max_depth=8,
+						subsample=0.18236,
+						max_leaves=0,
+						seed=42,)
 
-	print("Data prep done")
+time1 = time.time()
+model.fit(X_train, y_train)
 
-	model = xg.XGBRegressor(n_estimators=900,
-							learning_rate=0.008,
-							max_depth=8,
-							subsample=0.18236,
-							max_leaves=0,
-							seed=42,)
+print("Training complete")
 
-	time1 = time.time()
-	model.fit(X_train, y_train)
+predictions = model.predict(X_test) # XS predictions
+predictions_ReLU = []
+for pred in predictions:
+	if pred >= 0.001:
+		predictions_ReLU.append(pred)
+	else:
+		predictions_ReLU.append(0)
 
-	print("Training complete")
+predictions = predictions_ReLU
 
-	predictions = model.predict(X_test) # XS predictions
-	predictions_ReLU = []
-	for pred in predictions:
-		if pred >= 0.001:
-			predictions_ReLU.append(pred)
-		else:
-			predictions_ReLU.append(0)
+# Form arrays for plots below
+XS_plotmatrix = []
+E_plotmatrix = []
+P_plotmatrix = []
 
-	predictions = predictions_ReLU
+TENDL_XS_plotmatrix = []
+TENDL_E_plotmatrix = []
 
-	# Form arrays for plots below
-	XS_plotmatrix = []
-	E_plotmatrix = []
-	P_plotmatrix = []
+JENDL_XS_plotmatrix = []
+JENDL_E_plotmatrix = []
 
-	TENDL_XS_plotmatrix = []
-	TENDL_E_plotmatrix = []
+for nuclide in validation_nuclides:
+	dummy_test_XS = []
+	dummy_test_E = []
+	dummy_predictions = []
+	for i, row in enumerate(X_test):
+		if [row[0], row[1]] == nuclide:
+			dummy_test_XS.append(y_test[i])
+			dummy_test_E.append(row[4]) # Energy values are in 5th row
+			dummy_predictions.append(predictions[i])
 
-	JENDL_XS_plotmatrix = []
-	JENDL_E_plotmatrix = []
+	XS_plotmatrix.append(dummy_test_XS)
+	E_plotmatrix.append(dummy_test_E)
+	P_plotmatrix.append(dummy_predictions)
 
-	for nuclide in validation_nuclides:
-		dummy_test_XS = []
-		dummy_test_E = []
-		dummy_predictions = []
-		for i, row in enumerate(X_test):
-			if [row[0], row[1]] == nuclide:
-				dummy_test_XS.append(y_test[i])
-				dummy_test_E.append(row[4]) # Energy values are in 5th row
-				dummy_predictions.append(predictions[i])
+# plot predictions against data
+# note: python lists allow elements to be lists of varying lengths. This would not work using numpy arrays; the for
+# loop below loops through the lists ..._plotmatrix, where each element is a list corresponding to nuclide nuc[i].
+for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
 
-		XS_plotmatrix.append(dummy_test_XS)
-		E_plotmatrix.append(dummy_test_E)
-		P_plotmatrix.append(dummy_predictions)
+	nuc = validation_nuclides[i] # validation nuclide
 
-	# plot predictions against data
-	# note: python lists allow elements to be lists of varying lengths. This would not work using numpy arrays; the for
-	# loop below loops through the lists ..._plotmatrix, where each element is a list corresponding to nuclide nuc[i].
-	for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
+	jendl_erg, jendl_xs = General_plotter(df=JENDL, nuclides=[nuc])
+	tendl_erg, tendl_xs = General_plotter(df=TENDL, nuclides=[nuc])
 
-		nuc = validation_nuclides[i] # validation nuclide
+	plt.plot(erg, pred_xs, label='Predictions', color='red')
+	plt.plot(tendl_erg, tendl_xs, label = "TENDL21", color='dimgrey')
+	if len(jendl_erg) > 1:
+		plt.plot(jendl_erg, jendl_xs, label ='JENDL5', color='green')
+	plt.title(f"$\sigma_{{n,2n}}$ for {periodictable.elements[nuc[0]]}-{nuc[1]:0.0f}")
+	plt.legend()
+	plt.grid()
+	plt.ylabel('$\sigma_{n,2n}$ / b')
+	plt.xlabel('Energy / MeV')
+	plt.show()
 
-		jendl_erg, jendl_xs = General_plotter(df=JENDL, nuclides=[nuc])
-		tendl_erg, tendl_xs = General_plotter(df=TENDL, nuclides=[nuc])
+	r2 = r2_score(true_xs, pred_xs) # R^2 score for this specific nuclide
+	print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
+	time.sleep(0.5)
 
-		plt.plot(erg, pred_xs, label='Predictions', color='red')
-		plt.plot(tendl_erg, tendl_xs, label = "TENDL21", color='dimgrey')
-		if len(jendl_erg) > 1:
-			plt.plot(jendl_erg, jendl_xs, label ='JENDL5', color='green')
-		plt.title(f"$\sigma_{{n,2n}}$ for {periodictable.elements[nuc[0]]}-{nuc[1]:0.0f}")
-		plt.legend()
-		plt.grid()
-		plt.ylabel('$\sigma_{n,2n}$ / b')
-		plt.xlabel('Energy / MeV')
-		plt.show()
+	print(f"Turning points: {dsigma_dE(XS=pred_xs)}")
 
-		r2 = r2_score(true_xs, pred_xs) # R^2 score for this specific nuclide
-		print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
-		time.sleep(0.5)
-
-		print(f"Turning points: {dsigma_dE(XS=pred_xs)}")
-
-	print(f"MSE: {mean_squared_error(y_test, predictions, squared=False)}") # MSE
-	print(f"R2: {r2_score(y_test, predictions)}") # Total R^2 for all predictions in this training campaign
-	print(f'completed in {time.time() - time1} s')
+print(f"MSE: {mean_squared_error(y_test, predictions, squared=False)}") # MSE
+print(f"R2: {r2_score(y_test, predictions)}") # Total R^2 for all predictions in this training campaign
+print(f'completed in {time.time() - time1} s')
