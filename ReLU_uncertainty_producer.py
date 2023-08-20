4d4c2df29213ac984a60b4232faf44331a8f9a24
T G <rnt26@cam.ac.uk>
2023-08-20 02:17:39 +0100

    several additions

--- ReLU_uncertainty_producer.py
+++ ReLU_uncertainty_producer.py
@@ -36,7 +36,7 @@ al = range_setter(la=30, ua=215, df=df)
 
 n_evaluations = 100
 datapoint_matrix = []
-target_nuclide = [76,186]
+target_nuclide = [26,56]
 
 for i in tqdm.tqdm(range(n_evaluations)):
 	print(f"\nRun {i+1}/{n_evaluations}")
@@ -62,7 +62,7 @@ for i in tqdm.tqdm(range(n_evaluations)):
 	model_seed = random.randint(a=1, b=1000) # seed for subsampling
 
 	model = xg.XGBRegressor(n_estimators=900,
-							learning_rate=0.01,
+							learning_rate=0.008,
 							max_depth=8,
 							subsample=0.18236,
 							max_leaves=0,
@@ -76,7 +76,7 @@ for i in tqdm.tqdm(range(n_evaluations)):
 	predictions = model.predict(X_test) # XS predictions
 	predictions_ReLU = []
 	for pred in predictions:
-		if pred >= 0.001:
+		if pred >= 0.003:
 			predictions_ReLU.append(pred)
 		else:
 			predictions_ReLU.append(0)
@@ -210,112 +210,449 @@ if target_nuclide in JENDL_nuclides:
 
 	pred_jendl_mse = mean_squared_error(pred_jendl, jendl_xs)
 	pred_jendl_r2 = r2_score(y_true=jendl_xs, y_pred=pred_jendl)
-	print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f} MSE: {pred_jendl_mse:0.6f}")
+	print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f}")
 
 if target_nuclide in JEFF_nuclides:
 	jeff_test, jeff_xs = make_test(nuclides=[target_nuclide], df=JEFF33)
 
 	pred_jeff = model.predict(jeff_test)
 
-	pred_jeff_mse = mean_squared_error(pred_jeff, jeff_xs)
 	pred_jeff_r2 = r2_score(y_true=jeff_xs, y_pred=pred_jeff)
-	print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f} MSE: {pred_jeff_mse:0.6f}")
+	print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f}")
 
 if target_nuclide in TENDL_nuclides:
 	tendl_test, tendl_xs = make_test(nuclides=[target_nuclide], df=TENDL)
 	pred_tendl = model.predict(tendl_test)
 
-	pred_tendl_mse = mean_squared_error(pred_tendl, tendl_xs)
 	pred_tendl_r2 = r2_score(y_true=tendl_xs, y_pred=pred_tendl)
-	print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f} MSE: {pred_tendl_mse:0.6f}")
+	print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f}")
 
 
 
-Veeser_energies = [1.47000E+01,
-1.60000E+01,
-1.70000E+01,
+# Veeser_energies = [1.47000E+01,
+# 1.60000E+01,
+# 1.70000E+01,
+# 1.80000E+01,
+# 1.90000E+01,
+# 2.00000E+01,]
+# Veeser_delta_energies = [7.50000E-02,
+# 1.00000E-01,
+# 1.00000E-01,
+# 7.50000E-02,
+# 7.50000E-02,
+# 5.50000E-02,]
+#
+# Veeser_XS = [1.98400E+03,
+# 1.99200E+03,
+# 1.78700E+03,
+# 1.32000E+03,
+# 9.57000E+02,
+# 7.70000E+02,] # in mb
+#
+# Veeser_XS = [xs / 1000 for xs in Veeser_XS]
+# Veeser_delta_XS = [1.15000E+02,
+# 1.05000E+02,
+# 8.50000E+01,
+# 8.80000E+01,
+# 6.40000E+01,
+# 1.04000E+02,]
+#
+# Veeser_delta_XS = [delxs / 1000 for delxs in Veeser_delta_XS]
+#
+#
+# Dzysiuk_energies = []
+# Dzysiuk_delta_energies = []
+#
+# Dzysiuk_delta_XS = []
+# Dzysiuk_delta_XS = [xs / 1000 for xs in Dzysiuk_delta_XS]
+# Dzysiuk_XS = []
+# Dzysiuk_XS = [xs / 1000 for xs in Dzysiuk_XS]
+#
+# Bayhurst_XS = [3.23000E+02,
+# 8.16000E+02,
+# 1.78900E+03,
+# 1.66800E+03,
+# 1.33700E+03,
+# 9.65000E+02,
+# 5.74000E+02,]
+# Bayhurst_delta_XS = [1.50000E+01,
+# 3.50000E+01,
+# 7.60000E+01,
+# 7.10000E+01,
+# 0.00000E+00,
+# 0.00000E+00,
+# 0.00000E+00,]
+# Bayhurst_XS = [xs/1000 for xs in Bayhurst_XS]
+# Bayhurst_delta_XS = [xs/1000 for xs in Bayhurst_delta_XS]
+#
+#
+# Bayhurst_energies = [8.51000E+00,
+# 9.27000E+00,
+# 1.41000E+01,
+# 1.49200E+01,
+# 1.71900E+01,
+# 1.81900E+01,
+# 1.99400E+01,]
+# Bayhurst_delta_energies = [3.40000E-01,
+# 3.60000E-01,
+# 5.00000E-02,
+# 5.00000E-02,
+# 1.90000E-01,
+# 1.30000E-01,
+# 1.50000E-01,]
+#
+# Frehaut_E = [8.44000E+00,8.94000E+00,9.44000E+00,9.93000E+00,1.04200E+01,1.09100E+01,1.14000E+01,1.18800E+01,1.23600E+01,1.28500E+01,1.33300E+01,1.38000E+01,
+# 1.42800E+01,1.47600E+01]
+#
+# Frehaut_E_d = [1.35000E-01,1.25000E-01,1.15000E-01,1.10000E-01,1.00000E-01,9.50000E-02,9.00000E-02,
+# 8.50000E-02,8.50000E-02,8.00000E-02,7.50000E-02,7.50000E-02,7.00000E-02,6.50000E-02]
+#
+# Frehaut_XS = [2.03000E+02,6.59000E+02,1.16200E+03,1.40400E+03,1.56000E+03,1.72000E+03,1.69600E+03,1.81300E+03,1.91900E+03,1.99600E+03,2.03600E+03,
+# 2.07600E+03,2.11300E+03,2.09400E+03]
+#
+# Frehaut_XS_d = [3.30000E+01 ,5.00000E+01 ,1.12000E+02 ,8.60000E+01 ,9.50000E+01 ,1.24000E+02 ,1.07000E+02 ,1.10000E+02 ,1.21000E+02 ,1.25000E+02 ,1.26000E+02 ,1.23000E+02 ,1.55000E+02 ,1.57000E+02 ]
+#
+# Frehaut_XS = [xs/1000 for xs in Frehaut_XS]
+# Frehaut_XS_d = [xs/1000 for xs in Frehaut_XS_d]
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# LuE = [1.44000E+01]
+# LuXS = [ 1.440]
+# LudXS = [0.08]
+#
+# QaimE = [1.47000E+01]
+# QaimXS = [1.23000]
+# QaimdXS = [0.146]
+#
+# JunhuaE = [1.35000E+01,1.41000E+01,1.48000E+01]
+# JunhuaXS = [1.60000,1.73700,1.70700]
+# JunhuadXS = [0.061, 0.07, 0.01]
+#
+# TemperleyE = [1.41000E+01]
+# TemperleyXS = [1.57000]
+# TemperleydXS = [0.18]
+#
+# BormannE = [1.26600E+01,1.33900E+01,1.40900E+01,1.50900E+01,1.61900E+01,1.69500E+01,1.75800E+01,1.81300E+01]
+# BormannXS = [1.510,1.480,1.460,1.530,1.595,1.510,1.450,1.350]
+# BormanndXS = [0.14,.140,.145,.135,.160,.135,.140,.130]
+#
+#
+# #    E(MeV)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Frehaut_Pb_XS = [.8300,
+# .1710,
+# .3140,
+# .6050,
+# .9410,
+# 1.184,
+# 1.434,
+# 1.580,
+# 1.714,
+# 1.804,
+# 1.817,
+# 1.921,
+# 1.972,
+# 2.071,
+# 2.010,
+# 2.028]
+# Frehaut_Pb_E = [  7.93000E+00  ,
+#   8.18000E+00  ,
+#   8.44000E+00  ,
+#   8.94000E+00  ,
+#   9.44000E+00  ,
+#   9.93000E+00  ,
+#   1.04200E+01  ,
+#   1.09100E+01  ,
+#   1.14000E+01  ,
+#   1.18800E+01  ,
+#   1.23600E+01  ,
+#   1.28500E+01  ,
+#   1.33300E+01  ,
+#   1.38000E+01  ,
+#   1.42800E+01  ,
+#   1.47600E+01  ]
+# Frehaut_Pb_dXS = [0.0100,0.01400,0.02200,0.04000,.06100,.07500,.08300,.09700,.09800,.1050,.1370,.1430,.1500,.1600,
+# .1580,
+# .1630]
+#
+#
+#
+# FrehautW182_XS = [1.60000E+01,6.40000E+01,2.19000E+02,3.58000E+02,
+# 7.21000E+02,1.14400E+03,1.36600E+03,1.58200E+03,
+# 1.79000E+03,1.92300E+03,1.96000E+03,1.99000E+03,2.09200E+03,2.07600E+03]
+# FrehautW182_XS = [i /1000 for i in FrehautW182_XS]
+#
+# FrehautW182_E = [8.18000E+00,8.44000E+00,8.69000E+00,8.94000E+00,9.44000E+00,
+# 				 9.93000E+00,1.04200E+01,1.09100E+01,1.18800E+01,1.28500E+01,
+# 				 1.33300E+01,1.38000E+01,1.42800E+01,1.47600E+01]
+# FrehautW182_dXS = [1.20000E+01,1.20000E+01,2.80000E+01,2.20000E+01,4.10000E+01,6.00000E+01,7.00000E+01,8.00000E+01,
+# 1.27000E+02,1.37000E+02,1.38000E+02,1.42000E+02,1.53000E+02,1.45000E+02]
+# FrehautW182_dXS = [i / 1000 for i in FrehautW182_dXS]
+
+
+#    E(MeV)       xs(mb)      dxs(mb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Frehautnd_XS = [2.26000E+02 ,5.59000E+02 ,
+# 9.15000E+02 ,1.17300E+03 ,
+# 1.30300E+03 ,1.46700E+03 ,
+# 1.55600E+03 ,1.68800E+03 ,
+# 1.84400E+03 ,1.94600E+03 ,
+# 1.76200E+03 ]
+# Frehautnd_XS = [i/1000 for i in Frehautnd_XS]
+#
+# Frehautnddxs = [1.80000E+01 ,3.10000E+01 ,
+# 4.90000E+01 ,6.10000E+01 ,7.20000E+01 ,
+# 7.60000E+01 ,8.20000E+01 ,1.20000E+02 ,
+# 1.31000E+02 ,1.40000E+02 ,1.34000E+02 ]
+# Frehautnddxs = [i/1000 for i in Frehautnddxs]
+#
+# Frehaut_E = [8.03000E+00,8.44000E+00,8.94000E+00,9.44000E+00,9.93000E+00,1.04200E+01,
+# 1.09100E+01,1.18800E+01,1.28500E+01,1.38000E+01,1.47600E+01]
+#
+#
+#
+#
+# anxs = [1.92300E+03,
+# 1.76900E+03,
+# 1.86700E+03,
+# 1.67900E+03,
+# 1.58500E+03,
+# 1.42200E+03,
+# 1.36900E+03,
+# 1.34600E+03,
+# 8.75000E+02,
+# 7.62000E+02]
+# andxs = [1.73000E+02,
+# 1.41520E+02,
+# 1.49360E+02,
+# 1.34320E+02,
+# 1.26800E+02,
+# 1.13760E+02,
+# 1.81000E+02,
+# 1.40000E+02,
+# 8.60000E+01,
+# 7.90000E+01]
+# andxs = [x / 1000 for x in andxs]
+# anxs = [i/1000 for i in anxs]
+# ane = [1.30000E+01 ,
+# 1.34000E+01 ,
+# 1.39000E+01 ,
+# 1.45000E+01 ,
+# 1.50000E+01 ,
+# 1.54000E+01 ,
+# 1.59000E+01 ,
+# 1.66000E+01 ,
+# 1.74000E+01 ,
+# 1.79000E+01 ]
+#
+# KasugaiXS = [1.79000E+03,
+# 1.79000E+03,
+# 1.72000E+03,
+# 1.69000E+03,
+# 1.52000E+03,
+# 1.43000E+03]
+# KasugaiXS = [i/1000 for i in KasugaiXS]
+#
+# KasugaidXS = [1.20000E+02,
+# 1.20000E+02,
+# 1.10000E+02,
+# 1.10000E+02,
+# 1.00000E+02,
+# 1.00000E+02]
+# KasugaidXS = [i / 1000 for i in KasugaidXS]
+# Kasugai_E = [1.33800E+01,
+# 1.37000E+01,
+# 1.40300E+01,
+# 1.43600E+01,
+# 1.46600E+01,
+# 1.49500E+01]
+
+# junhua_XS = [1.24600,1.27900,1.29100]
+# junhua_XSd = [0.07,0.074,0.074]
+# junhua_E = [1.35000E+01,1.41000E+01,1.48000E+01]
+
+
+#    E(MeV)       xs(mb)      dxs(mb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# meghaxs = [4.49100E+01,
+# 2.14300E+02,
+# 3.55810E+02,
+# 5.28750E+02,
+# 6.60350E+02,
+# 8.00070E+02,
+# 8.81830E+02,
+# 9.41420E+02,
+# 1.02667E+03,
+# 1.07021E+03,
+# 1.09026E+03]
+# meghaxsd = [2.59000E+00,
+# 1.22200E+01,
+# 1.84600E+01,
+# 3.02300E+01,
+# 3.38200E+01,
+# 5.93200E+01,
+# 6.53800E+01,
+# 4.50200E+01,
+# 4.67800E+01,
+# 5.31800E+01,
+# 5.47100E+01]
+# meghaxsd = [i/1000 for i in meghaxsd]
+# meghaxs = [i/1000 for i in meghaxs]
+# meghae = [9.90000E+00,
+# 1.03900E+01,
+# 1.08900E+01,
+# 1.13900E+01,
+# 1.18800E+01,
+# 1.23800E+01,
+# 1.28700E+01,
+# 1.33700E+01,
+# 1.38700E+01,
+# 1.43600E+01,
+# 1.48000E+01]
+#
+#
+# puxs = [1.07900E+03,
+# 1.21700E+03,
+# 1.13900E+03]
+# puxsd = [8.80000E+01,
+# 1.02000E+02,
+# 9.20000E+01]
+# puxsd = [i/1000 for i in puxsd]
+# puxs = [i/1000 for i in puxs]
+# pue = [1.35000E+01,
+# 1.41000E+01,
+# 1.46000E+01]
+
+
+
+wallnerxs = [3.00000E+02,
+3.22400E+02,
+3.81300E+02,
+3.81200E+02,
+4.69200E+02,
+4.67200E+02,
+5.01500E+02,
+4.61100E+02,
+4.83900E+02,
+5.08300E+02,
+5.45400E+02,
+6.47000E+02,
+6.22000E+02,
+7.02000E+02,
+5.68000E+02]
+
+wallnerxs = [i/1000 for i in wallnerxs]
+
+wallnerxsd = [1.00000E+02,1.61000E+01,3.29000E+01,1.91000E+01,2.35000E+01,4.05000E+01,2.51000E+01,
+2.31000E+01,2.81000E+01,2.54000E+01,2.73000E+01,6.00000E+01,4.70000E+01,4.10000E+01,3.80000E+01]
+wallnerxsd = [i/1000 for i in wallnerxsd]
+
+wallnere = [1.33500E+01,
+1.34900E+01,
+1.36900E+01,
+1.39700E+01,
+1.42400E+01,
+1.46300E+01,
+1.46700E+01,
+1.46700E+01,
+1.46900E+01,
+1.47400E+01,
+1.48200E+01,
 1.80000E+01,
-1.90000E+01,
-2.00000E+01,]
-Veeser_delta_energies = [7.50000E-02,
-1.00000E-01,
-1.00000E-01,
-7.50000E-02,
-7.50000E-02,
-5.50000E-02,]
+1.80000E+01,
+1.83300E+01,
+2.06800E+01]
 
-Veeser_XS = [1.98400E+03,
-1.99200E+03,
-1.78700E+03,
-1.32000E+03,
-9.57000E+02,
-7.70000E+02,] # in mb
-
-Veeser_XS = [xs / 1000 for xs in Veeser_XS]
-Veeser_delta_XS = [1.15000E+02,
-1.05000E+02,
-8.50000E+01,
-8.80000E+01,
-6.40000E+01,
-1.04000E+02,]
-
-Veeser_delta_XS = [delxs / 1000 for delxs in Veeser_delta_XS]
+#    E(MeV)       xs(mb)      dxs(mb)
 
 
-Dzysiuk_energies = []
-Dzysiuk_delta_energies = []
-
-Dzysiuk_delta_XS = []
-Dzysiuk_delta_XS = [xs / 1000 for xs in Dzysiuk_delta_XS]
-Dzysiuk_XS = []
-Dzysiuk_XS = [xs / 1000 for xs in Dzysiuk_XS]
-
-Bayhurst_XS = [3.23000E+02,
-8.16000E+02,
-1.78900E+03,
-1.66800E+03,
-1.33700E+03,
-9.65000E+02,
-5.74000E+02,]
-Bayhurst_delta_XS = [1.50000E+01,
-3.50000E+01,
-7.60000E+01,
-7.10000E+01,
-0.00000E+00,
-0.00000E+00,
-0.00000E+00,]
-Bayhurst_XS = [xs/1000 for xs in Bayhurst_XS]
-Bayhurst_delta_XS = [xs/1000 for xs in Bayhurst_delta_XS]
 
 
-Bayhurst_energies = [8.51000E+00,
-9.27000E+00,
-1.41000E+01,
-1.49200E+01,
-1.71900E+01,
-1.81900E+01,
-1.99400E+01,]
-Bayhurst_delta_energies = [3.40000E-01,
-3.60000E-01,
-5.00000E-02,
-5.00000E-02,
-1.90000E-01,
-1.30000E-01,
-1.50000E-01,]
 
-Frehaut_E = [8.44000E+00,8.94000E+00,9.44000E+00,9.93000E+00,1.04200E+01,1.09100E+01,1.14000E+01,1.18800E+01,1.23600E+01,1.28500E+01,1.33300E+01,1.38000E+01,
-1.42800E+01,1.47600E+01]
 
-Frehaut_E_d = [1.35000E-01,1.25000E-01,1.15000E-01,1.10000E-01,1.00000E-01,9.50000E-02,9.00000E-02,
-8.50000E-02,8.50000E-02,8.00000E-02,7.50000E-02,7.50000E-02,7.00000E-02,6.50000E-02]
 
-Frehaut_XS = [2.03000E+02,6.59000E+02,1.16200E+03,1.40400E+03,1.56000E+03,1.72000E+03,1.69600E+03,1.81300E+03,1.91900E+03,1.99600E+03,2.03600E+03,
-2.07600E+03,2.11300E+03,2.09400E+03]
 
-Frehaut_XS_d = [3.30000E+01 ,5.00000E+01 ,1.12000E+02 ,8.60000E+01 ,9.50000E+01 ,1.24000E+02 ,1.07000E+02 ,1.10000E+02 ,1.21000E+02 ,1.25000E+02 ,1.26000E+02 ,1.23000E+02 ,1.55000E+02 ,1.57000E+02 ]
+# fxs = [3.70000E+01  ,
+# 1.04000E+02  ,
+# 2.13000E+02  ,
+# 3.16000E+02  ,
+# 4.02000E+02  ,
+# 4.10000E+02  ,
+# 5.19000E+02  ]
+# fxsd = [4.00000E+00 ,
+# 6.00000E+00 ,
+# 1.20000E+01 ,
+# 1.90000E+01 ,
+# 2.40000E+01 ,
+# 3.30000E+01 ,
+# 4.10000E+01 ]
+#
+# fxs = [i/1000 for i in fxs]
+# fxsd = [i/1000 for i in fxsd]
+#
+# fe = [1.18800E+01,
+# 1.23600E+01,
+# 1.28500E+01,
+# 1.33300E+01,
+# 1.38000E+01,
+# 1.42800E+01,
+# 1.47600E+01]
 
-Frehaut_XS = [xs/1000 for xs in Frehaut_XS]
-Frehaut_XS_d = [xs/1000 for xs in Frehaut_XS_d]
+
+
+
 
 
 title_string_latex = "$\sigma_{n,2n}$"
@@ -329,12 +666,27 @@ plt.plot(E_plot, datapoint_means, label = 'Prediction', color='red')
 plt.plot(E_plot, XS_plot, label = 'ENDF/B-VIII', linewidth=2)
 plt.plot(tendl_energy, tendl_xs, label = 'TENDL21', color='dimgrey')
 if target_nuclide in JEFF_nuclides:
-	plt.plot(JEFF_energy, JEFF_XS, label='JEFF3.3', color='mediumvioletred')
+	plt.plot(JEFF_energy, JEFF_XS, '--', label='JEFF3.3', color='mediumvioletred')
 if target_nuclide in JENDL_nuclides:
-	plt.plot(JENDL5_energy, JENDL5_XS, '--', label='JENDL5', color='green')
+	plt.plot(JENDL5_energy, JENDL5_XS, label='JENDL5', color='green')
 if target_nuclide in CENDL_nuclides:
 	plt.plot(CENDL32_energy, CENDL33_XS, '--', label = 'CENDL3.2', color='gold')
 plt.fill_between(E_plot, datapoint_lower_interval, datapoint_upper_interval, alpha=0.2, label='95% CI', color='red')
+plt.errorbar(fe, fxs, yerr=fxsd, fmt='x', color='indigo',capsize=2, label='Frehaut, 1980')
+plt.errorbar(wallnere, wallnerxs, yerr=wallnerxsd, fmt='x', color='indigo',capsize=2, label='Wallner, 2011')
+
+# plt.errorbar(pue, puxs, yerr=puxsd, fmt='x', color='indigo',capsize=2, label='Pu, 2006')
+# plt.errorbar(meghae, meghaxs, yerr=meghaxsd, fmt='x', color='violet',capsize=2, label='Megha, 2017')
+# plt.errorbar(junhua_E, junhua_XS, yerr=junhua_XSd, fmt='x', color='orangered',capsize=2, label='Junhua, 2018')
+# plt.errorbar(FrehautW182_E, FrehautW182_XS, yerr=FrehautW182_dXS,
+# 			 fmt='x', color='indigo',capsize=2, label='Frehaut, 1980')
+# plt.errorbar(Frehaut_Pb_E, Frehaut_Pb_XS, yerr=Frehaut_Pb_dXS, fmt='x', color='indigo',capsize=2, label='Frehaut, 1980')
+# plt.errorbar(LuE, LuXS, yerr=LudXS, fmt='x', color='indigo',capsize=2, label='Lu, 1970')
+# plt.errorbar(QaimE, QaimXS, yerr=QaimdXS, fmt='x', color='violet',capsize=2, label='Qaim, 1974')
+# plt.errorbar(JunhuaE, JunhuaXS, yerr=JunhuadXS, fmt='x', color='blue',capsize=2, label='JunhuaLuoo, 2007')
+# plt.errorbar(TemperleyE, TemperleyXS, yerr=TemperleydXS, fmt='x', color='orangered',capsize=2, label='Temperley, 1970')
+# plt.errorbar(BormannE, BormannXS, yerr=BormanndXS, fmt='x',capsize=2, label='Bormann, 1970')
+
 # plt.errorbar(Bayhurst_energies, Bayhurst_XS, Bayhurst_delta_XS, Bayhurst_delta_energies, fmt='x',
 # 			 capsize=2, label='Bayhurst, 1975', color='indigo')
 # plt.errorbar(Frehaut_E, Frehaut_XS, Frehaut_XS_d, Frehaut_E_d, fmt='x',
