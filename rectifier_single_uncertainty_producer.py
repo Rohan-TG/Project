0777d2d3f3d79d459d30d36f761e90ceccb3ce7c
T G <rnt26@cam.ac.uk>
2023-10-21 22:02:23 +0100

    moved stuff

--- rectifier_single_uncertainty_producer.py
+++ rectifier_single_uncertainty_producer.py
@@ -12,7 +12,7 @@ import time
 from sklearn.metrics import mean_squared_error, r2_score
 import periodictable
 from matrix_functions import dsigma_dE, make_train, make_test,\
-	range_setter, General_plotter
+	range_setter, General_plotter, r2_standardiser
 import scipy.stats
 from datetime import timedelta
 import tqdm
@@ -31,12 +31,38 @@ df.index = range(len(df))
 
 al = range_setter(la=30, ua=215, df=df)
 
+TENDL = pd.read_csv("TENDL_2021_MT16_XS_features.csv")
+TENDL.index = range(len(TENDL))
+TENDL_nuclides = range_setter(df=TENDL, la=30, ua=210)
+
+
+JEFF33 = pd.read_csv('JEFF33_all_features.csv')
+JEFF33.index = range(len(JEFF33))
+JEFF_nuclides = range_setter(df=JEFF33, la=30, ua=210)
+
+
+JENDL5 = pd.read_csv('JENDL5_arange_all_features.csv')
+JENDL5.index = range(len(JENDL5))
+JENDL_nuclides = range_setter(df=JENDL5, la=30, ua=210)
+
+
+CENDL32 = pd.read_csv('CENDL32_all_features.csv')
+CENDL32.index = range(len(CENDL32))
+CENDL_nuclides = range_setter(df=CENDL32, la=30, ua=210)
 
 
 
-n_evaluations = 50
+n_evaluations = 30
 datapoint_matrix = []
-target_nuclide = [40,95]
+target_nuclide = [26,56]
+
+jendlerg, jendlxs = General_plotter(df=JENDL5, nuclides=[target_nuclide])
+cendlerg, cendlxs = General_plotter(df=CENDL32, nuclides=[target_nuclide])
+jefferg, jeffxs = General_plotter(df=JEFF33, nuclides=[target_nuclide])
+tendlerg, tendlxs = General_plotter(df=TENDL, nuclides=[target_nuclide])
+endfberg, endfbxs = General_plotter(df=df, nuclides=[target_nuclide])
+
+runs_r2_array = []
 
 for i in tqdm.tqdm(range(n_evaluations)):
 	print(f"\nRun {i+1}/{n_evaluations}")
@@ -67,7 +93,6 @@ for i in tqdm.tqdm(range(n_evaluations)):
 							subsample=0.18236,
 							max_leaves=0,
 							seed=model_seed,)
-
 
 	model.fit(X_train, y_train)
 
@@ -113,18 +138,68 @@ for i in tqdm.tqdm(range(n_evaluations)):
 		E_plotmatrix.append(dummy_test_E)
 		P_plotmatrix.append(dummy_predictions)
 
-	# plot predictions against data
-	# note: python lists allow elements to be lists of varying lengths. This would not work using numpy arrays; the for
-	# loop below loops through the lists ..._plotmatrix, where each element is a list corresponding to nuclide nuc[i].
-	for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
-		nuc = validation_nuclides[i] # validation nuclide
-		r2 = r2_score(true_xs, pred_xs) # R^2 score for this specific nuclide
+	all_preds = []
+	all_libs = []
+
+
+	pred_endfb_r2 = r2_score(y_true=XS_plotmatrix[0], y_pred=P_plotmatrix[0])
+	for x, y in zip(XS_plotmatrix[0], P_plotmatrix[0]):
+		all_libs.append(y)
+		all_preds.append(x)
+	print(f"Predictions - ENDF/B-VIII R2: {pred_endfb_r2:0.5f} ")
+
+	if target_nuclide in CENDL_nuclides:
+
+		cendl_test, cendl_xs = make_test(nuclides=[target_nuclide], df=CENDL32)
+		pred_cendl = model.predict(cendl_test)
+
+		pred_cendl_r2 = r2_score(y_true=cendl_xs, y_pred=pred_cendl)
+		for x, y in zip(pred_cendl, cendl_xs):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - CENDL3.2 R2: {pred_cendl_r2:0.5f}")
+
+	if target_nuclide in JENDL_nuclides:
+
+		jendl_test, jendl_xs = make_test(nuclides=[target_nuclide], df=JENDL5)
+		pred_jendl = model.predict(jendl_test)
+
+		pred_jendl_r2 = r2_score(y_true=jendl_xs, y_pred=pred_jendl)
+		print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f}")
+		for x, y in zip(pred_jendl, jendl_xs):
+			all_libs.append(y)
+			all_preds.append(x)
+
+	if target_nuclide in JEFF_nuclides:
+		jeff_test, jeff_xs = make_test(nuclides=[target_nuclide], df=JEFF33)
+
+		pred_jeff = model.predict(jeff_test)
+
+		pred_jeff_gated, truncated_jeff, pred_jeff_r2 = r2_standardiser(raw_predictions=pred_jeff, library_xs=jeff_xs)
+		for x, y in zip(pred_jeff_gated, truncated_jeff):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f}")
+
+	if target_nuclide in TENDL_nuclides:
+
+		tendl_test, tendl_xs = make_test(nuclides=[target_nuclide], df=TENDL)
+		pred_tendl = model.predict(tendl_test)
+
+		pred_tendl_mse = mean_squared_error(pred_tendl, tendl_xs)
+		pred_tendl_r2 = r2_score(y_true=tendl_xs, y_pred=pred_tendl)
+		for x, y in zip(pred_tendl, tendl_xs):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f}")
+	consensus = r2_score(all_libs, all_preds)
+
+	runs_r2_array.append(consensus)
+	print(f"Consensus R2: {r2_score(all_libs, all_preds):0.5f}")
 
 	exp_true_xs = [y for y in y_test]
 	exp_pred_xs = [xs for xs in predictions]
 
-	print(f"MSE: {mean_squared_error(exp_true_xs, exp_pred_xs, squared=False)}") # MSE
-	print(f"R2: {r2_score(exp_true_xs, exp_pred_xs):0.5f}") # Total R^2 for all predictions in this training campaign
 	print(f'completed in {time.time() - time1:0.1f} s')
 
 
@@ -165,67 +240,6 @@ for point, up, low, in zip(datapoint_means, datapoint_upper_interval, datapoint_
 
 
 
-TENDL = pd.read_csv("TENDL21_MT16_XS_features_zeroed.csv")
-TENDL.index = range(len(TENDL))
-TENDL_nuclides = range_setter(df=TENDL, la=30, ua=210)
-tendl_energy, tendl_xs = General_plotter(df=TENDL, nuclides=[target_nuclide])
-
-
-JEFF33 = pd.read_csv('JEFF33_all_features.csv')
-JEFF33.index = range(len(JEFF33))
-JEFF_nuclides = range_setter(df=JEFF33, la=30, ua=210)
-JEFF_energy, JEFF_XS = General_plotter(df=JEFF33, nuclides=[target_nuclide])
-
-
-JENDL5 = pd.read_csv('JENDL5_arange_all_features.csv')
-JENDL5.index = range(len(JENDL5))
-JENDL5_energy, JENDL5_XS = General_plotter(df=JENDL5, nuclides=[target_nuclide])
-JENDL_nuclides = range_setter(df=JENDL5, la=30, ua=210)
-
-
-CENDL32 = pd.read_csv('CENDL32_all_features.csv')
-CENDL32.index = range(len(CENDL32))
-CENDL32_energy, CENDL33_XS = General_plotter(df=CENDL32, nuclides=[target_nuclide])
-CENDL_nuclides = range_setter(df=CENDL32, la=30, ua=210)
-mse = mean_squared_error(y_true=true_xs, y_pred=pred_xs)
-r2 = r2_score(true_xs, pred_xs)  # R^2 score for this specific nuclide
-print(f"\n{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}, MSE: {mse:0.5f}")
-time.sleep(0.8)
-
-pred_endfb_mse = mean_squared_error(pred_xs, true_xs)
-pred_endfb_r2 = r2_score(y_true=true_xs, y_pred=pred_xs)
-print(f"Predictions - ENDF/B-VIII R2: {pred_endfb_r2:0.5f} MSE: {pred_endfb_mse:0.6f}")
-
-if target_nuclide in CENDL_nuclides:
-	cendl_test, cendl_xs = make_test(nuclides=[target_nuclide], df=CENDL32)
-	pred_cendl = model.predict(cendl_test)
-
-	pred_cendl_mse = mean_squared_error(pred_cendl, cendl_xs)
-	pred_cendl_r2 = r2_score(y_true=cendl_xs, y_pred=pred_cendl)
-	print(f"Predictions - CENDL3.2 R2: {pred_cendl_r2:0.5f} MSE: {pred_cendl_mse:0.6f}")
-
-if target_nuclide in JENDL_nuclides:
-	jendl_test, jendl_xs = make_test(nuclides=[target_nuclide], df=JENDL5)
-	pred_jendl = model.predict(jendl_test)
-
-	pred_jendl_mse = mean_squared_error(pred_jendl, jendl_xs)
-	pred_jendl_r2 = r2_score(y_true=jendl_xs, y_pred=pred_jendl)
-	print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f}")
-
-if target_nuclide in JEFF_nuclides:
-	jeff_test, jeff_xs = make_test(nuclides=[target_nuclide], df=JEFF33)
-
-	pred_jeff = model.predict(jeff_test)
-
-	pred_jeff_r2 = r2_score(y_true=jeff_xs, y_pred=pred_jeff)
-	print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f}")
-
-if target_nuclide in TENDL_nuclides:
-	tendl_test, tendl_xs = make_test(nuclides=[target_nuclide], df=TENDL)
-	pred_tendl = model.predict(tendl_test)
-
-	pred_tendl_r2 = r2_score(y_true=tendl_xs, y_pred=pred_tendl)
-	print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f}")
 
 
 
@@ -638,13 +652,13 @@ print(f"Turning points: {dsigma_dE(XS=datapoint_means)}")
 #2sigma CF
 plt.plot(E_plot, datapoint_means, label = 'Prediction', color='red')
 plt.plot(E_plot, XS_plot, label = 'ENDF/B-VIII', linewidth=2)
-plt.plot(tendl_energy, tendl_xs, label = 'TENDL21', color='dimgrey')
+plt.plot(tendlerg, tendl_xs, label = 'TENDL21', color='dimgrey')
 if target_nuclide in JEFF_nuclides:
-	plt.plot(JEFF_energy, JEFF_XS, '--', label='JEFF3.3', color='mediumvioletred')
+	plt.plot(jefferg, jeffxs, '--', label='JEFF3.3', color='mediumvioletred')
 if target_nuclide in JENDL_nuclides:
-	plt.plot(JENDL5_energy, JENDL5_XS, label='JENDL5', color='green')
+	plt.plot(jendlerg, jendlxs, label='JENDL5', color='green')
 if target_nuclide in CENDL_nuclides:
-	plt.plot(CENDL32_energy, CENDL33_XS, '--', label = 'CENDL3.2', color='gold')
+	plt.plot(cendlerg, cendlxs, '--', label = 'CENDL3.2', color='gold')
 plt.fill_between(E_plot, datapoint_lower_interval, datapoint_upper_interval, alpha=0.2, label='95% CI', color='red')
 # plt.errorbar(cse, csxs, yerr=csxsd, fmt='x', color='violet',capsize=2, label='Csikai, 1967')
 # plt.errorbar(tiwarie, tiwarixs, yerr=tiwarixsd,
@@ -681,7 +695,16 @@ plt.ylabel("$\sigma_{n,2n}$ / b")
 plt.legend(loc='upper left')
 plt.show()
 
+mu_r2 = np.mean(runs_r2_array)
+sigma_r2 = np.std(runs_r2_array)
+interval_low_r2, interval_high_r2 = scipy.stats.t.interval(0.95, loc=mu_r2, scale=sigma_r2, df=(len(runs_r2_array) - 1))
+print(f"\nConsensus: {mu_r2:0.5f} +/- {interval_high_r2-interval_low_r2:0.5f}")
+
+
 
 final_runtime = time.time() - runtime
 
+
+
+print()
 print(f"Runtime: {timedelta(seconds=final_runtime)}")
