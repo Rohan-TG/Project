497106164c78cac681cc66e826cdd70691924f45
T G <rnt26@cam.ac.uk>
2023-10-23 21:56:20 +0100

    updated r2 calculation

--- endf_high_comparator.py
+++ endf_high_comparator.py
@@ -9,7 +9,7 @@ import xgboost as xg
 import time
 from sklearn.metrics import mean_squared_error, r2_score
 import periodictable
-from matrix_functions import make_test, make_train, anomaly_remover, range_setter, General_plotter
+from matrix_functions import make_test, make_train, anomaly_remover, range_setter, General_plotter, r2_standardiser
 
 df_test = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
 df = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
@@ -21,7 +21,7 @@ df.index = range(len(df))
 df_test = anomaly_remover(dfa = df_test)
 al = range_setter(la=210, ua=260, df=df)
 
-TENDL = pd.read_csv("TENDL21_MT16_XS_features_zeroed.csv")
+TENDL = pd.read_csv("TENDL_2021_MT16_XS_features.csv")
 TENDL.index = range(len(TENDL))
 TENDL_nuclides = range_setter(df=TENDL, la=210, ua=260)
 
@@ -38,7 +38,7 @@ CENDL.index = range(len(CENDL))
 CENDL_nuclides = range_setter(df=CENDL, la=210, ua=260)
 
 validation_nuclides = []
-validation_set_size = 10 # number of nuclides hidden from training
+validation_set_size = 5 # number of nuclides hidden from training
 
 # random.seed(a=42)
 
@@ -104,8 +104,69 @@ for nuclide in validation_nuclides:
 # note: python lists allow elements to be lists of varying lengths. This would not work using numpy arrays; the for
 # loop below loops through the lists ..._plotmatrix, where each element is a list corresponding to nuclide nuc[i].
 for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
+	all_libs = []
+	all_preds = []
 
 	current_nuclide = validation_nuclides[i]
+
+	pred_endfb_gated, truncated_endfb, pred_endfb_r2 = r2_standardiser(raw_predictions=pred_xs, library_xs=true_xs)
+	for x, y in zip(pred_endfb_gated, truncated_endfb):
+		all_libs.append(y)
+		all_preds.append(x)
+	print(f"Predictions - ENDF/B-VIII R2: {pred_endfb_r2:0.5f}")
+
+	# Find r2 w.r.t. other libraries
+	if current_nuclide in CENDL_nuclides:
+
+		cendl_test, cendl_xs = make_test(nuclides=[current_nuclide], df=CENDL)
+		pred_cendl = model.predict(cendl_test)
+
+		pred_cendl_gated, truncated_cendl, pred_cendl_r2 = r2_standardiser(raw_predictions=pred_cendl, library_xs=cendl_xs)
+
+		pred_cendl_mse = mean_squared_error(pred_cendl, cendl_xs)
+		for x, y in zip(pred_cendl_gated, truncated_cendl):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - CENDL3.2 R2: {pred_cendl_r2:0.5f} MSE: {pred_cendl_mse:0.6f}")
+
+	if current_nuclide in JENDL_nuclides:
+
+		jendl_test, jendl_xs = make_test(nuclides=[current_nuclide], df=JENDL)
+		pred_jendl = model.predict(jendl_test)
+
+		pred_jendl_mse = mean_squared_error(pred_jendl, jendl_xs)
+		pred_jendl_gated, truncated_jendl, pred_jendl_r2 = r2_standardiser(raw_predictions=pred_jendl, library_xs=jendl_xs)
+
+		for x, y in zip(pred_jendl_gated, truncated_jendl):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f} MSE: {pred_jendl_mse:0.6f}")
+
+	if current_nuclide in JEFF_nuclides:
+		jeff_test, jeff_xs = make_test(nuclides=[current_nuclide], df=JEFF)
+
+		pred_jeff = model.predict(jeff_test)
+
+		pred_jeff_mse = mean_squared_error(pred_jeff, jeff_xs)
+		pred_jeff_gated, truncated_jeff, pred_jeff_r2 = r2_standardiser(raw_predictions=pred_jeff, library_xs=jeff_xs)
+		for x, y in zip(pred_jeff_gated, truncated_jeff):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f} MSE: {pred_jeff_mse:0.6f}")
+
+	if current_nuclide in TENDL_nuclides:
+
+		tendl_test, tendl_xs = make_test(nuclides=[current_nuclide], df=TENDL)
+		pred_tendl = model.predict(tendl_test)
+
+		pred_tendl_mse = mean_squared_error(pred_tendl, tendl_xs)
+		pred_tendl_gated, truncated_tendl, pred_tendl_r2 = r2_standardiser(raw_predictions=pred_tendl, library_xs=tendl_xs)
+		for x, y in zip(pred_tendl_gated, truncated_tendl):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f} MSE: {pred_tendl_mse:0.6f}")
+
+
 
 	jendlerg, jendlxs = General_plotter(df=JENDL, nuclides=[current_nuclide])
 	cendlerg, cendlxs = General_plotter(df=CENDL, nuclides=[current_nuclide])
@@ -129,8 +190,8 @@ for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_p
 	plt.xlabel('Energy / MeV')
 	plt.show()
 
-	r2 = r2_score(true_xs, pred_xs) # R^2 score for this specific nuclide
-	print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}")
+	r2 = r2_score(all_libs, all_preds) # R^2 score for this specific nuclide
+	print(f"{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}\n")
 	time.sleep(0.5)
 
 print(f"MSE: {mean_squared_error(y_test, predictions, squared=False):0.5f}") # MSE
@@ -138,9 +199,3 @@ print(f"R2: {r2_score(y_test, predictions):0.5f}") # Total R^2 for all predictio
 print(f'completed in {time.time() - time1:0.1f} s')
 
 
-train_predictions = model.predict(X_train)
-
-train_r2 = r2_score(y_true=y_train, y_pred=train_predictions)
-train_mse = mean_squared_error(y_true=y_train, y_pred=train_predictions)
-print(f"Training R2: {train_r2:0.5f}")
-print(f"Training MSE: {train_mse:0.5f}")
