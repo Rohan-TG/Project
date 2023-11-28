3718b3c69ceda3c197f0ea57313de343f3492252
T G <rnt26@cam.ac.uk>
2023-11-28 13:44:36 +0000

    fixed energies

--- matrix_functions.py
+++ matrix_functions.py
@@ -351,7 +351,7 @@ def make_train(df, validation_nuclides, la=0, ua=260):
 	for idx, unused in enumerate(Z):  # MT = 16 is (n,2n) (already extracted)
 		if [Z[idx], A[idx]] in validation_nuclides:
 			continue # prevents loop from adding test isotope data to training data
-		if Energy[idx] >= 21: # training on data less than 30 MeV
+		if Energy[idx] > 20: # training on data less than 30 MeV
 			continue
 		if A[idx] <= ua and A[idx] >= la: # checks that nuclide is within bounds for A
 			Z_train.append(Z[idx])
