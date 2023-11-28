2a1dbd9114eac5108786d1a8868f2b6453b5dcbe
Rohan-TG <rohan@MSI>
2023-11-28 13:50:54 +0000

    test

--- MT_103_regression/MT_103_regressor.py
+++ MT_103_regression/MT_103_regressor.py
@@ -5,6 +5,8 @@ import xgboost
 import periodictable
 import random
 import time
+import shap
+
 
 df = pd.read_csv('ENDFBVIII_MT103_fund_features_only.csv')
 
@@ -12,7 +14,7 @@ ENDFB_nuclides = range_setter(df=df, la=0, ua=260)
 print("Data loaded")
 
 validation_nuclides = []
-validation_set_size = 10
+validation_set_size = 20
 
 while len(validation_nuclides) < validation_set_size:
 	nuclide_choice = random.choice(ENDFB_nuclides) # randomly select nuclide from list of all nuclides in ENDF/B-VIII
@@ -20,13 +22,14 @@ while len(validation_nuclides) < validation_set_size:
 		validation_nuclides.append(nuclide_choice)
 print("Test nuclide selection complete")
 
-X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=0, ua=260)
+X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=0, ua=100)
 X_test, y_test = make_test(validation_nuclides, df=df)
 
-model = xgboost.XGBRegressor(n_estimators = 900,
-							 learning_rate = 0.008,
+model = xgboost.XGBRegressor(n_estimators = 200,
+							 learning_rate = 0.1,
 							 max_depth = 8,
-							 subsample = 0.2
+							 subsample = 0.2,
+							 reg_lambda = 0
 							 )
 
 model.fit(X_train, y_train)
@@ -69,4 +72,83 @@ for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_p
 	plt.xlabel('Energy / MeV')
 	plt.show()
 
-	time.sleep(1)
+	time.sleep(1.5)
+
+explainer = shap.Explainer(model.predict, X_train,
+						   feature_names= ['Z',
+									 'A',
+									 'S2n',
+									 'S2p',
+									 'E',
+									 'Sp',
+									 'Sn',
+									 'BEA',
+									 'P',
+									 'Snc',
+									 'g-def',
+									 'N',
+									 'b-def',
+									 'Sn da',
+									 'Sp d',
+									 'S2n d',
+									 'Radius',
+									 'n_g_erg',
+									 'n_c_erg',
+									 'n_rms_r',
+									 'oct_def',
+									 'D_c',
+									 'BEA_d',
+									 'BEA_c',
+									 'Pair_d',
+									 'Par_d',
+									 'S2n_c',
+									 'S2p_c',
+									 'ME',
+									 'Z_even',
+									 'A_even',
+									 'N_even',
+									 'Shell',
+									 'Parity',
+									 'Spin',
+									 'Decay',
+									 'Deform',
+									 'p_g_e',
+									 'p_c_e',
+									 'p_rms_r',
+									 'rms_r',
+									 'Sp_c',
+									 'Sn_c',
+									 'Shell_c',
+									 'S2p-d',
+									 'Shell-d',
+									 'Spin-c',
+									 'Rad-c',
+									 'Def-c',
+									 'ME-c',
+									 'BEA-A-c',
+									 'Decay-d',
+									 'ME-d',
+									 'Rad-d',
+									 'Pair-c',
+									 'Par-c',
+									 'BEA-A-d',
+									 'Spin-d',
+									 'Def-d',
+									 # 'mag_p',
+									 # 'mag-n',
+									 # 'mag-d',
+									 # 'Nlow',
+									 # 'Ulow',
+									 # 'Ntop',
+									 # 'Utop',
+									 # 'ainf',
+									 # 'Asym',
+									 # 'Asym_c',
+									 # 'Asym_d',
+									 # 'AM'
+									 ]) # SHAP feature importance analysis
+shap_values = explainer(X_test)
+
+
+
+shap.plots.bar(shap_values, max_display = 70) # display SHAP results
