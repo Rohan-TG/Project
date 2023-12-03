eac66f56acdd0b6e67f0404a90c00c0dcc351bb3
T G <rnt26@cam.ac.uk>
2023-12-03 16:48:06 +0000

    WIP

--- Data_handling/crude_MT103_MT107_feature_inserter.py
+++ Data_handling/crude_MT103_MT107_feature_inserter.py
@@ -22,8 +22,20 @@ count = 0
 
 interpolated_MT103 = pd.DataFrame(columns=['Z', 'A', 'ERG', 'XS'])
 
-for nuclide in ENDF_B_nuclides:
+
+
+ENDFBVIII_MT16['MT103XS'] = pd.Series(index=ENDFBVIII_MT16.index)
+ENDFBVIII_MT16['MT107XS'] = pd.Series(index=ENDFBVIII_MT16.index)
+
+main_columns = ENDFBVIII_MT16.columns
+new_df = pd.DataFrame(columns=main_columns)
+
+MT103_df = pd.DataFrame(columns=['Z', 'A', 'MT103XS', 'ERG'])
+
+for num, nuclide in enumerate(ENDF_B_nuclides):
+	print(f"{num}/{len(ENDF_B_nuclides)}")
 	if nuclide in nucs103:
+
 		erg103, xs103 = General_plotter(df=ENDFBVIII_MT103, nuclides=[nuclide])
 		erg16, xs16 = General_plotter(df=ENDFBVIII_MT16, nuclides=[nuclide])
 
@@ -35,19 +47,34 @@ for nuclide in ENDF_B_nuclides:
 
 		ixs103 = f103(erg16)
 
-		# plt.figure()
-		# plt.grid()
-		# plt.plot(erg103, xs103, label = 'original')
-		# plt.plot(erg16, ixs103, label = 'interpolated')
-		# plt.legend()
-		# plt.xlabel('Energy / MeV')
-		# plt.ylabel('$\sigma_{n,p}$ / b')
-		# plt.title(f"(n,p) cross sections for {periodictable.elements[nuclide[0]]}-{nuclide[1]}")
-		# plt.show()
-		# time.sleep(1)
-		# count +=1
-		# if count > 20:
-		# 	break
+		for xs103, i_erg  in zip(ixs103, erg16):
+			xs103_row = pd.Series({'Z': nuclide[0], 'A': nuclide[1], 'MT103XS': xs103, 'ERG': i_erg})
+			MT103_df = MT103_df._append(xs103_row, ignore_index=True)
+
+
+
+
+
+# for MT16_row in ENDFBVIII_MT16.iterrows():
+# 	for MT103_row in MT103_df.iterrows():
+# 		if [MT103_row['Z'], MT103_row['A']] == [MT16_row['Z'], MT16_row['A']] and MT103_row['ERG'] == MT16_row['ERG']:
+# 			dummy_row = MT16_row
+# 			dummy_row['MT103XS'] = MT103_row['MT103XS']
+			# MT16_row['MT103XS'] = MT103_row['MT103XS']
+#
+# 		# plt.figure()
+# 		# plt.grid()
+# 		# plt.plot(erg103, xs103, label = 'original')
+# 		# plt.plot(erg16, ixs103, label = 'interpolated')
+# 		# plt.legend()
+# 		# plt.xlabel('Energy / MeV')
+# 		# plt.ylabel('$\sigma_{n,p}$ / b')
+# 		# plt.title(f"(n,p) cross sections for {periodictable.elements[nuclide[0]]}-{nuclide[1]}")
+# 		# plt.show()
+# 		# time.sleep(1)
+# 		# count +=1
+# 		# if count > 20:
+# 		# 	break
 
 # logxs = [np.log(i) for i in nucmass[2]]
 # plt.figure()
