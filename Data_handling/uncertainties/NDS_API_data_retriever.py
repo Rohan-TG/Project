fc18ade8714ee1b1ddf46dfe6e492fc6dbdebb5e
T G <rnt26@cam.ac.uk>
2024-01-27 14:23:18 +0000

    done for now

--- Data_handling/uncertainties/NDS_API_data_retriever.py
+++ Data_handling/uncertainties/NDS_API_data_retriever.py
@@ -3,8 +3,8 @@ import periodictable
 
 
 
-url = 'https://www-nds.iaea.org/relnsd/v1/data?fields=levels&nuclides=135xe'
-#
+# url = 'https://www-nds.iaea.org/relnsd/v1/data?fields=levels&nuclides=135xe'
+url = 'https://www-nds.iaea.org/relnsd/v1/data?fields=ground_states&nuclides=all'
 
 
 
