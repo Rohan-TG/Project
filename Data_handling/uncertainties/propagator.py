6913b512f4b1662e5a3424d02c36508bc90e653a
T G <rnt26@cam.ac.uk>
2024-01-30 17:08:55 +0000

    still a WIP

--- Data_handling/uncertainties/propagator.py
+++ Data_handling/uncertainties/propagator.py
@@ -2,7 +2,9 @@ import pandas as pd
 import xgboost as xg
 import matplotlib.pyplot as plt
 from matrix_functions import range_setter
-from propagator_functions import training_sampler
+from propagator_functions import training_sampler, make_test
+from sklearn.metrics import r2_score, mean_squared_error
+import time
 
 ENDFBVIII = pd.read_csv('ENDFBVIII_MT16_uncertainties.csv')
 ENDFB_nuclides = range_setter(df=ENDFBVIII, la=0, ua= 210)
@@ -25,9 +27,160 @@ CENDL_nuclides = range_setter(df=CENDL, la=0, ua=210)
 
 
 
-target_nuclide = []
-n_evaluations = 10
+target_nuclides = [[71,175]]
+n_evaluations = 3
+
+datapoint_matrix = []
 
 for i in range(n_evaluations):
 
-	X_train, y_train = training_sampler(df=ENDFBVIII, LA=30, UA=210, sampled_uncertainties= ['unc_sn'])
+	X_train, y_train = training_sampler(df=ENDFBVIII, LA=30, UA=210, sampled_uncertainties= ['unc_sn'], target_nuclides=target_nuclides)
+	X_test, y_test = make_test(nuclides=target_nuclides, df=ENDFBVIII)
+
+	print('Data prep complete')
+
+	model = xg.XGBRegressor(n_estimators=900,  # define regressor
+							learning_rate=0.008,
+							max_depth=8,
+							subsample=0.18236,
+							max_leaves=0,
+							seed=42, )
+
+	predictions = model.predict(X_test) # XS predictions
+	predictions_ReLU = []
+	for pred in predictions:
+		if pred >= 0.003:
+			predictions_ReLU.append(pred)
+		else:
+			predictions_ReLU.append(0)
+
+	predictions = predictions_ReLU
+
+	if i == 0:
+		for k, pred in enumerate(predictions):
+			if [X_test[k,0], X_test[k,1]] == target_nuclides[0]:
+				datapoint_matrix.append([pred])
+	else:
+		valid_predictions = []
+		for k, pred in enumerate(predictions):
+			if [X_test[k, 0], X_test[k, 1]] == target_nuclides[0]:
+				valid_predictions.append(pred)
+		for m, prediction in zip(datapoint_matrix, valid_predictions):
+			m.append(prediction)
+
+	XS_plotmatrix = []
+	E_plotmatrix = []
+	P_plotmatrix = []
+
+	for nuclide in target_nuclides:
+		dummy_test_XS = []
+		dummy_test_E = []
+		dummy_predictions = []
+		for i, row in enumerate(X_test):
+			if [row[0], row[1]] == nuclide:
+				dummy_test_XS.append(y_test[i])
+				dummy_test_E.append(row[4])  # Energy values are in 5th row
+				dummy_predictions.append(predictions[i])
+
+		XS_plotmatrix.append(dummy_test_XS)
+		E_plotmatrix.append(dummy_test_E)
+		P_plotmatrix.append(dummy_predictions)
+
+		all_preds = []
+		all_libs = []
+
+	pred_endfb_r2 = r2_score(y_true=XS_plotmatrix[0], y_pred=P_plotmatrix[0])
+	for x, y in zip(XS_plotmatrix[0], P_plotmatrix[0]):
+		all_libs.append(y)
+		all_preds.append(x)
+	print(f"Predictions - ENDF/B-VIII R2: {pred_endfb_r2:0.5f} ")
+
+	if target_nuclide in CENDL_nuclides:
+
+		cendl_test, cendl_xs = make_test(nuclides=[target_nuclide], df=CENDL32)
+		pred_cendl = model.predict(cendl_test)
+
+		pred_cendl_r2 = r2_score(y_true=cendl_xs, y_pred=pred_cendl)
+		for x, y in zip(pred_cendl, cendl_xs):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - CENDL3.2 R2: {pred_cendl_r2:0.5f}")
+
+	if target_nuclide in JENDL_nuclides:
+
+		jendl_test, jendl_xs = make_test(nuclides=[target_nuclide], df=JENDL5)
+		pred_jendl = model.predict(jendl_test)
+
+		pred_jendl_r2 = r2_score(y_true=jendl_xs, y_pred=pred_jendl)
+		print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f}")
+		for x, y in zip(pred_jendl, jendl_xs):
+			all_libs.append(y)
+			all_preds.append(x)
+
+	if target_nuclide in JEFF_nuclides:
+		jeff_test, jeff_xs = make_test(nuclides=[target_nuclide], df=JEFF33)
+
+		pred_jeff = model.predict(jeff_test)
+
+		pred_jeff_gated, truncated_jeff, pred_jeff_r2 = r2_standardiser(raw_predictions=pred_jeff,
+																		library_xs=jeff_xs)
+		for x, y in zip(pred_jeff_gated, truncated_jeff):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f}")
+
+	if target_nuclide in TENDL_nuclides:
+
+		tendl_test, tendl_xs = make_test(nuclides=[target_nuclide], df=TENDL)
+		pred_tendl = model.predict(tendl_test)
+
+		pred_tendl_mse = mean_squared_error(pred_tendl, tendl_xs)
+		pred_tendl_r2 = r2_score(y_true=tendl_xs, y_pred=pred_tendl)
+		for x, y in zip(pred_tendl, tendl_xs):
+			all_libs.append(y)
+			all_preds.append(x)
+		print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f}")
+	consensus = r2_score(all_libs, all_preds)
+
+	runs_r2_array.append(consensus)
+	print(f"Consensus R2: {r2_score(all_libs, all_preds):0.5f}")
+
+	exp_true_xs = [y for y in y_test]
+	exp_pred_xs = [xs for xs in predictions]
+
+	print(f'completed in {time.time() - time1:0.1f} s')
+
+XS_plot = []
+E_plot = []
+
+for i, row in enumerate(X_test):
+	if [row[0], row[1]] == target_nuclide:
+		XS_plot.append(y_test[i])
+		E_plot.append(row[4]) # Energy values are in 5th row
+
+
+datapoint_means = []
+datapoint_upper_interval = []
+datapoint_lower_interval = []
+
+d_l_1sigma = []
+d_u_1sigma = []
+
+for point in datapoint_matrix:
+
+	mu = np.mean(point)
+	sigma = np.std(point)
+	interval_low, interval_high = scipy.stats.t.interval(0.95, loc=mu, scale=sigma, df=(len(point) - 1))
+	# il_1sigma, ih_1sigma = scipy.stats.t.interval(0.68, loc=mu, scale=sigma, df=(len(point) - 1))
+	datapoint_means.append(mu)
+	datapoint_upper_interval.append(interval_high)
+	datapoint_lower_interval.append(interval_low)
+
+	# d_l_1sigma.append(il_1sigma)
+	# d_u_1sigma.append(ih_1sigma)
+
+lower_bound = []
+upper_bound = []
+for point, up, low, in zip(datapoint_means, datapoint_upper_interval, datapoint_lower_interval):
+	lower_bound.append(point-low)
+	upper_bound.append(point+up)
