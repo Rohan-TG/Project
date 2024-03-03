4bf754216e74b96a51b5ba2eb43eb4be093382da
RNTG <rohan.tg05@gmail.com>
2024-03-03 17:10:44 +0000

    useable version

--- Data_handling/Library_comparisons.py
+++ Data_handling/Library_comparisons.py
@@ -9,37 +9,41 @@ import numpy as np
 import time
 
 
-TENDL = pd.read_csv("TENDL21_MT16_XS_features_zeroed.csv")
+TENDL = pd.read_csv("TENDL_2021_MT16_XS_features.csv")
 TENDL.index = range(len(TENDL))
-TENDL_nuclides = range_setter(df=TENDL, la=215, ua=270)
+TENDL_nuclides = range_setter(df=TENDL, la=0, ua=210)
 
-JEFF = pd.read_csv('JEFF33_features_arange_zeroed.csv')
+JEFF = pd.read_csv('JEFF33_all_features.csv')
 JEFF.index = range(len(JEFF))
-JEFF_nuclides = range_setter(df=JEFF, la=215, ua=270)
+JEFF_nuclides = range_setter(df=JEFF, la=0, ua=210)
 
 JENDL = pd.read_csv('JENDL5_arange_all_features.csv')
 JENDL.index = range(len(JENDL))
-JENDL_nuclides = range_setter(df=JENDL, la=215, ua=270)
+JENDL_nuclides = range_setter(df=JENDL, la=0, ua=210)
 
-CENDL = pd.read_csv('CENDL32_features_arange_zeroed.csv')
+CENDL = pd.read_csv('CENDL32_all_features.csv')
 CENDL.index = range(len(CENDL))
-CENDL_nuclides = range_setter(df=CENDL, la=215, ua=270)
+CENDL_nuclides = range_setter(df=CENDL, la=0, ua=210)
 
 ENDFB_VIII = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
 ENDFB_VIII.index = range(len(ENDFB_VIII))
-ENDFB_VIII_nuclides = range_setter(df=ENDFB_VIII, la=215, ua=270)
+ENDFB_VIII_nuclides = range_setter(df=ENDFB_VIII, la=0, ua=210)
 
 
-nucs = []
-for i in range(20):
-    nucs.append(random.choice(ENDFB_VIII_nuclides))
-
+nucs = [[17,36]]
+set_size = 25
+while len(nucs) < set_size:
+    choice = random.choice(ENDFB_VIII_nuclides)
+    if choice not in nucs:
+        nucs.append(random.choice(ENDFB_VIII_nuclides))
 
 
 
 for i in nucs:
 
     current_nuclide = i
+    auxilliary_library_comparisons = []
+    endfb_comparisons = []
 
     endfberg, endfbxs = General_plotter(df=ENDFB_VIII, nuclides=[current_nuclide])
     jendlerg, jendlxs = General_plotter(df=JENDL, nuclides=[current_nuclide])
@@ -51,11 +55,11 @@ for i in nucs:
     plt.plot(endfberg, endfbxs, label='ENDF/B-VIII', linewidth=2)
     plt.plot(tendlerg, tendlxs, label="TENDL21", color='dimgrey', linewidth=2)
     if nuc in JEFF_nuclides:
-        plt.plot(jefferg, jeffxs, '--', label='JEFF3.3', color='mediumvioletred')
+        plt.plot(jefferg, jeffxs, '--', label='JEFF-3.3', color='mediumvioletred')
     if nuc in JENDL_nuclides:
-        plt.plot(jendlerg, jendlxs, label='JENDL5', color='green')
+        plt.plot(jendlerg, jendlxs, label='JENDL-5', color='green')
     if nuc in CENDL_nuclides:
-        plt.plot(cendlerg, cendlxs, '--', label='CENDL3.2', color='gold')
+        plt.plot(cendlerg, cendlxs, '--', label='CENDL-3.2', color='gold')
     plt.title(f"$\sigma_{{n,2n}}$ for {periodictable.elements[current_nuclide[0]]}-{current_nuclide[1]}")
     plt.legend()
     plt.grid()
@@ -63,7 +67,7 @@ for i in nucs:
     plt.xlabel('Energy / MeV')
     plt.show()
 
-    time.sleep(0.8)
+    time.sleep(0.7)
 
     x_interpolate = np.linspace(start=0.0, stop=19.4, num=200)
 
@@ -73,52 +77,85 @@ for i in nucs:
     if current_nuclide in nucs:
         endfb8_erg, endfb8_xs = General_plotter(df=ENDFB_VIII, nuclides=[current_nuclide])
 
-        f_endfb8 = scipy.interpolate.interp1d(x=endfb8_erg, y=endfb8_xs)
+        f_endfb8 = scipy.interpolate.interp1d(x=endfb8_erg, y=endfb8_xs, fill_value='extrapolate')
         endfb8_interp_xs = f_endfb8(x_interpolate)
 
-    if current_nuclide in CENDL_nuclides:
-        cendl_erg, cendl_xs = General_plotter(df=CENDL, nuclides=[current_nuclide])
-
-
-        f_cendl32 = scipy.interpolate.interp1d(x=cendl_erg, y=cendl_xs)
-        cendl_interp_xs = f_cendl32(x_interpolate)
 
     if current_nuclide in TENDL_nuclides:
         tendl_erg, tendl_xs = General_plotter(df=TENDL, nuclides=[current_nuclide])
 
-        f_tendl21 = scipy.interpolate.interp1d(x=tendl_erg, y=tendl_xs)
+        f_tendl21 = scipy.interpolate.interp1d(x=tendl_erg, y=tendl_xs, fill_value='extrapolate')
         tendl_interp_xs = f_tendl21(x_interpolate)
 
     if current_nuclide in JEFF_nuclides:
         jeff_erg, jeff_xs = General_plotter(df=JEFF, nuclides=[current_nuclide])
 
-        f_jeff33 = scipy.interpolate.interp1d(x=jeff_erg, y=jeff_xs)
+        f_jeff33 = scipy.interpolate.interp1d(x=jeff_erg, y=jeff_xs, fill_value='extrapolate')
         jeff_interp_xs = f_jeff33(x_interpolate)
 
-        rmse_tendl_jeff = r2_score(jeff_interp_xs, tendl_interp_xs)
-        rmse_endfb_jeff = r2_score(endfb8_interp_xs, jeff_interp_xs)
+        r2_tendl_jeff = r2_score(jeff_interp_xs, tendl_interp_xs)
+        r2_endfb_jeff = r2_score(endfb8_interp_xs, jeff_interp_xs)
 
-        print(f"TENDL21 - JEFF3.3: {rmse_tendl_jeff:0.6f}")
-        print(f"ENDF/B-VIII - JEFF3.3: {rmse_endfb_jeff:0.6f}")
+        print(f"TENDL21 - JEFF3.3: {r2_tendl_jeff:0.6f}")
+        auxilliary_library_comparisons.append(r2_tendl_jeff)
+        print(f"ENDF/B-VIII - JEFF3.3: {r2_endfb_jeff:0.6f}")
+        endfb_comparisons.append(r2_endfb_jeff)
+
 
     if current_nuclide in JENDL_nuclides:
         jendl_erg, jendl_xs = General_plotter(df=JENDL, nuclides=[current_nuclide])
         if max(jendl_erg) > 18:
-            f_jendl5 = scipy.interpolate.interp1d(x=jendl_erg, y=jendl_xs)
+            f_jendl5 = scipy.interpolate.interp1d(x=jendl_erg, y=jendl_xs, fill_value='extrapolate')
             jendl_interp_xs = f_jendl5(x_interpolate)
 
-            rmse_endf_jendl = r2_score(endfb8_interp_xs, jendl_interp_xs)
-            rmse_tendl_jendl = r2_score(jendl_interp_xs, tendl_interp_xs)
-            rmse_jeff_jendl = r2_score(jeff_interp_xs, jendl_interp_xs)
+            r2_endf_jendl = r2_score(endfb8_interp_xs, jendl_interp_xs)
+            r2_tendl_jendl = r2_score(jendl_interp_xs, tendl_interp_xs)
+            r2_jeff_jendl = r2_score(jeff_interp_xs, jendl_interp_xs)
 
-            print(f"ENDF/B-VIII - JENDL5: {rmse_endf_jendl:0.6f}")
+            print(f"ENDF/B-VIII - JENDL5: {r2_endf_jendl:0.6f}")
+            endfb_comparisons.append(r2_endf_jendl)
             if nuc in JEFF_nuclides:
-                print(f"JEFF3.3 - JENDL5: {rmse_jeff_jendl:0.6f}")
-            print(f"TENDL21 - JENDL5: {rmse_tendl_jendl:0.6f}")
+                print(f"JEFF3.3 - JENDL5: {r2_jeff_jendl:0.6f}")
+                auxilliary_library_comparisons.append(r2_jeff_jendl)
+            print(f"TENDL21 - JENDL5: {r2_tendl_jendl:0.6f}")
+            auxilliary_library_comparisons.append(r2_tendl_jendl)
+
+    if current_nuclide in CENDL_nuclides:
+        cendl_erg, cendl_xs = General_plotter(df=CENDL, nuclides=[current_nuclide])
+
+        f_cendl32 = scipy.interpolate.interp1d(x=cendl_erg, y=cendl_xs, fill_value='extrapolate')
+        cendl_interp_xs = f_cendl32(x_interpolate)
+
+        r2_endf_cendl = r2_score(endfb8_interp_xs, cendl_interp_xs)
+        endfb_comparisons.append(r2_endf_cendl)
+        print(f"ENDF/B - CENDL32: {r2_endf_cendl:0.6f}")
+        r2_tendl_cendl = r2_score(tendl_interp_xs, cendl_interp_xs)
+        auxilliary_library_comparisons.append(r2_tendl_cendl)
+        print(f"TENDL - CENDL32: {r2_tendl_cendl:0.6f}")
+        if current_nuclide in JENDL_nuclides:
+            r2_jendl_cendl = r2_score(jendl_interp_xs, cendl_interp_xs)
+            auxilliary_library_comparisons.append(r2_jendl_cendl)
+            print(f"JENDL5 - CENDL32: {r2_jendl_cendl:0.6f}")
+        if current_nuclide in JEFF_nuclides:
+            r2_jeff_cendl = r2_score(jeff_interp_xs, cendl_interp_xs)
+            print(f"JEFF33 - CENDL32: {r2_jeff_cendl:0.6f}")
+            auxilliary_library_comparisons.append(r2_jeff_cendl)
 
 
 
-    rmse_endf_tendl = r2_score(endfb8_interp_xs, tendl_interp_xs)
+    r2_endf_tendl = r2_score(endfb8_interp_xs, tendl_interp_xs)
+    endfb_comparisons.append(r2_endf_tendl)
 
-    print(f"ENDF/B-VIII - TENDL21: {rmse_endf_tendl:0.6f}")
-    print()
+    endfmean = np.mean(endfb_comparisons)
+    endfstd = np.std(endfb_comparisons)
+
+    auxmean = np.mean(auxilliary_library_comparisons)
+    auxstd = np.std(auxilliary_library_comparisons)
+
+    print(f"ENDF/B-VIII - TENDL21: {r2_endf_tendl:0.6f}")
+    print(f"Aux mean/std: {auxmean} $\pm$ {auxstd}")
+    print(f"ENDF/B mean/std: {endfmean} +- {endfstd}")
+
+    if abs((endfmean - auxmean)) > auxstd:
+        print(f"Outlier: {current_nuclide} - ({periodictable.elements[current_nuclide[0]]}-{current_nuclide[1]})")
+
