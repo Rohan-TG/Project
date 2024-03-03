06bed64cf3d6bd8160419a48768654f47675488f
RNTG <rohan.tg05@gmail.com>
2024-03-03 17:34:39 +0000

    improvements

--- Data_handling/Library_comparisons.py
+++ Data_handling/Library_comparisons.py
@@ -11,33 +11,33 @@ import time
 
 TENDL = pd.read_csv("TENDL_2021_MT16_XS_features.csv")
 TENDL.index = range(len(TENDL))
-TENDL_nuclides = range_setter(df=TENDL, la=0, ua=210)
+TENDL_nuclides = range_setter(df=TENDL, la=30, ua=210)
 
 JEFF = pd.read_csv('JEFF33_all_features.csv')
 JEFF.index = range(len(JEFF))
-JEFF_nuclides = range_setter(df=JEFF, la=0, ua=210)
+JEFF_nuclides = range_setter(df=JEFF, la=30, ua=210)
 
 JENDL = pd.read_csv('JENDL5_arange_all_features.csv')
 JENDL.index = range(len(JENDL))
-JENDL_nuclides = range_setter(df=JENDL, la=0, ua=210)
+JENDL_nuclides = range_setter(df=JENDL, la=30, ua=210)
 
 CENDL = pd.read_csv('CENDL32_all_features.csv')
 CENDL.index = range(len(CENDL))
-CENDL_nuclides = range_setter(df=CENDL, la=0, ua=210)
+CENDL_nuclides = range_setter(df=CENDL, la=30, ua=210)
 
 ENDFB_VIII = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
 ENDFB_VIII.index = range(len(ENDFB_VIII))
-ENDFB_VIII_nuclides = range_setter(df=ENDFB_VIII, la=0, ua=210)
+ENDFB_VIII_nuclides = range_setter(df=ENDFB_VIII, la=30, ua=210)
 
 
-nucs = [[17,36]]
+nucs = []
 set_size = 25
 while len(nucs) < set_size:
     choice = random.choice(ENDFB_VIII_nuclides)
     if choice not in nucs:
-        nucs.append(random.choice(ENDFB_VIII_nuclides))
+        nucs.append(choice)
 
-
+outliers = []
 
 for i in nucs:
 
@@ -67,7 +67,7 @@ for i in nucs:
     plt.xlabel('Energy / MeV')
     plt.show()
 
-    time.sleep(0.7)
+    time.sleep(0.3)
 
     x_interpolate = np.linspace(start=0.0, stop=19.4, num=200)
 
@@ -104,21 +104,21 @@ for i in nucs:
 
     if current_nuclide in JENDL_nuclides:
         jendl_erg, jendl_xs = General_plotter(df=JENDL, nuclides=[current_nuclide])
-        if max(jendl_erg) > 18:
-            f_jendl5 = scipy.interpolate.interp1d(x=jendl_erg, y=jendl_xs, fill_value='extrapolate')
-            jendl_interp_xs = f_jendl5(x_interpolate)
 
-            r2_endf_jendl = r2_score(endfb8_interp_xs, jendl_interp_xs)
-            r2_tendl_jendl = r2_score(jendl_interp_xs, tendl_interp_xs)
-            r2_jeff_jendl = r2_score(jeff_interp_xs, jendl_interp_xs)
+        f_jendl5 = scipy.interpolate.interp1d(x=jendl_erg, y=jendl_xs, fill_value='extrapolate')
+        jendl_interp_xs = f_jendl5(x_interpolate)
 
-            print(f"ENDF/B-VIII - JENDL5: {r2_endf_jendl:0.6f}")
-            endfb_comparisons.append(r2_endf_jendl)
-            if nuc in JEFF_nuclides:
-                print(f"JEFF3.3 - JENDL5: {r2_jeff_jendl:0.6f}")
-                auxilliary_library_comparisons.append(r2_jeff_jendl)
-            print(f"TENDL21 - JENDL5: {r2_tendl_jendl:0.6f}")
-            auxilliary_library_comparisons.append(r2_tendl_jendl)
+        r2_endf_jendl = r2_score(endfb8_interp_xs, jendl_interp_xs)
+        r2_tendl_jendl = r2_score(jendl_interp_xs, tendl_interp_xs)
+        r2_jeff_jendl = r2_score(jeff_interp_xs, jendl_interp_xs)
+
+        print(f"ENDF/B-VIII - JENDL5: {r2_endf_jendl:0.6f}")
+        endfb_comparisons.append(r2_endf_jendl)
+        if nuc in JEFF_nuclides:
+            print(f"JEFF3.3 - JENDL5: {r2_jeff_jendl:0.6f}")
+            auxilliary_library_comparisons.append(r2_jeff_jendl)
+        print(f"TENDL21 - JENDL5: {r2_tendl_jendl:0.6f}")
+        auxilliary_library_comparisons.append(r2_tendl_jendl)
 
     if current_nuclide in CENDL_nuclides:
         cendl_erg, cendl_xs = General_plotter(df=CENDL, nuclides=[current_nuclide])
@@ -156,8 +156,14 @@ for i in nucs:
     print(f"Aux mean/std: {auxmean} $\pm$ {auxstd}")
     print(f"ENDF/B mean/std: {endfmean} +- {endfstd}")
 
-    if abs((endfmean - auxmean)) > auxstd:
+    if len(auxilliary_library_comparisons) < 2:
+        if abs((endfmean - auxmean)) > 0.02:
+            print(f"Outlier: {current_nuclide} - ({periodictable.elements[current_nuclide[0]]}-{current_nuclide[1]})")
+            outliers.append(current_nuclide)
+    elif abs((endfmean - auxmean)) > auxstd and len(auxilliary_library_comparisons) > 1:
         print(f"Outlier: {current_nuclide} - ({periodictable.elements[current_nuclide[0]]}-{current_nuclide[1]})")
+        outliers.append(current_nuclide)
 
     print()
 
+print(outliers)
