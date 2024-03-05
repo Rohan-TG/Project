be23660f1fce1939c58b20b5b0b88e2c79647a38
RNTG <rohan.tg05@gmail.com>
2024-03-05 22:44:39 +0000

    wip

---
+++ multilibrary/multi_library_benchmarker.py
@@ -0,0 +1,36 @@
+import warnings
+from numba.core.errors import NumbaDeprecationWarning
+warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
+import pandas as pd
+import random
+import xgboost as xg
+import time
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+from matrix_functions import make_train, make_test, range_setter, r2_standardiser
+
+df = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
+df_test = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
+df_test.index = range(len(df_test)) # re-label indices
+df.index = range(len(df))
+
+al = range_setter(df=df, la=30, ua=210)
+
+TENDL = pd.read_csv("TENDL_2021_MT16_XS_features.csv")
+TENDL.index = range(len(TENDL))
+TENDL_nuclides = range_setter(df=TENDL, la=0, ua=260)
+
+JEFF = pd.read_csv('JEFF33_all_features.csv')
+JEFF.index = range(len(JEFF))
+JEFF_nuclides = range_setter(df=JEFF, la=0, ua=260)
+
+JENDL = pd.read_csv('JENDL5_arange_all_features.csv')
+JENDL.index = range(len(JENDL))
+JENDL_nuclides = range_setter(df=JENDL, la=0, ua=260)
+
+CENDL = pd.read_csv('CENDL32_all_features.csv')
+CENDL.index = range(len(CENDL))
+CENDL_nuclides = range_setter(df=CENDL, la=0, ua=260)
+
+n_run_tally95 = []
+
