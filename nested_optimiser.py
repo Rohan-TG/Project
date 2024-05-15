a2709e7cecc9abf8c1f9f46d8e3c9be0f3ecaf4c
RNTG <rohan.tg05@gmail.com>
2024-05-15 16:18:37 +0100

    wip for janis optimisation

--- nested_optimiser.py
+++ nested_optimiser.py
@@ -5,47 +5,46 @@ import periodictable
 from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
 from hyperopt.pyll.base import scope
 import hyperopt.early_stop
-from matrix_functions import anomaly_remover, OPT_make_train, OPT_make_test
+from matrix_functions import make_train, make_test, range_setter, r2_standardiser
 import xgboost as xg
 from sklearn.metrics import r2_score, mean_squared_error
 
 
-df = pd.read_csv("ENDFBVIII_zeroed_LDP_XS.csv")
-df_test = pd.read_csv("ENDFBVIII_zeroed_LDP_XS.csv")
+df = pd.read_csv("ENDFBVIII_JANIS_features_MT16.csv")
 
-al = []
-for i, j, in zip(df['A'], df['Z']): # i is A, j is Z
-	if [j, i] in al or i > 210:
-		continue
-	else:
-		al.append([j, i]) # format is [Z, A]
+al = range_setter(df=df, la=30, ua=210)
 
 
-
-space = {'n_estimators': hp.choice('n_estimators', [500, 600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1200, ]),
+space = {'n_estimators': hp.choice('n_estimators', [500, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1200, 1250, 1300, 1350, 1400]),
 		 'subsample': hp.loguniform('subsample', np.log(0.05), np.log(1.0)),
 		 'max_leaves': 0,
 		 'max_depth': scope.int(hp.quniform("max_depth", 6, 12, 1)),
-		 'learning_rate': hp.loguniform('learning_rate', np.log(0.001), np.log(0.03))}
+		 'learning_rate': hp.loguniform('learning_rate', np.log(0.0005), np.log(0.03))}
 
 
-
-benchmark_number = 3
+benchmark_number = 20
 nuclide_mse = []
 nuclide_r2 = []
 
 every_prediction_list = []
 every_true_value_list = []
 
-
+exc = [[22, 47], [65, 159], [66, 157], [38, 90], [61, 150],
+	   [74, 185], [50, 125], [50, 124], [60, 149], [39, 90],
+	   [64, 160], [38, 87], [39, 91], [63, 152], [52, 125],
+	   [19, 40], [56, 139], [52, 126], [71, 175], [34, 79],
+	   [70, 175], [50, 117], [23, 49], [63, 156], [57, 140],
+	   [52, 128], [59, 142], [50, 118], [50, 123], [65, 161],
+	   [52, 124], [38, 85], [51, 122], [19, 41], [54, 135],
+	   [32, 75], [81, 205], [71, 176], [72, 175], [50, 122],
+	   [51, 125], [53, 133], [34, 82], [41, 95], [46, 109],
+	   [84, 209], [56, 140], [64, 159], [68, 167], [16, 35],
+	   [18,38], [44,99], [50,126]]
 
 def optimiser(space):
 
 	validation_nuclides = []
-	validation_set_size = 10
-
-	test_nuclides = [[26, 56], [78, 195], [24, 52], [92, 235], [92, 238],
-					 [40, 90], ]
+	validation_set_size = 20
 
 
 	for i in range(benchmark_number):
@@ -59,9 +58,9 @@ def optimiser(space):
 				nuclides_used.append(choice)
 		print("Nuclide selection complete")
 
-		X_train, y_train = OPT_make_train(df=df, val_nuc=validation_nuclides, test_nuc=test_nuclides)
+		X_train, y_train = make_train(df=df, exclusions=exc, validation_nuclides=nuclides_used)
 
-		X_test, y_test = OPT_make_test(nuclides=validation_nuclides, df=df_test)
+		X_test, y_test = make_test(nuclides=validation_nuclides, df=df)
 		print("Data prep done")
 
 		model = xg.XGBRegressor(**space, seed=42)
@@ -69,10 +68,10 @@ def optimiser(space):
 		evaluation = [(X_train, y_train), (X_test, y_test)]
 
 		model.fit(X_train, y_train,
-				  eval_set=evaluation, eval_metric='rmse')
+				  eval_set=evaluation, eval_metric='rmse',
+				  verbose = True)
 
 		predictions = model.predict(X_test)
-
 
 		XS_plotmatrix = []
 		E_plotmatrix = []
@@ -109,7 +108,7 @@ def optimiser(space):
 			for val in y_test:
 				every_true_value_list.append(val)
 
-	HP_SET_R2 = r2_score(every_true_value_list, every_prediction_list) # for one iteration of the k-fold cross validation
+	HP_SET_R2 = r2_standardiser(raw_predictions=every_prediction_list, library_xs=every_true_value_list) # for one iteration of the k-fold cross validation
 
 	target_r2 = 1 - HP_SET_R2
 
@@ -121,8 +120,8 @@ best = fmin(fn=optimiser,
 			space=space,
 			algo=tpe.suggest,
 			trials=trials,
-			max_evals=70,
-			early_stop_fn=hyperopt.early_stop.no_progress_loss(50))
+			max_evals=100,
+			early_stop_fn=hyperopt.early_stop.no_progress_loss(20))
 
 # print(best)
 
