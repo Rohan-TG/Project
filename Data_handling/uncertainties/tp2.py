3502a70d2ef5c99236c2dd0bc81a6742e178dba0
RNTG <rohan.tg05@gmail.com>
2024-06-07 13:33:04 +0100

    sn

--- Data_handling/uncertainties/tp2.py
+++ Data_handling/uncertainties/tp2.py
@@ -38,7 +38,7 @@ CENDL_nuclides = range_setter(df=CENDL, la=0, ua=210)
 exc = exclusion_func() # 10 sigma with handpicked additions
 
 
-target_nuclides = [[54,135]]
+target_nuclides = [[50,118]]
 target_nuclide = target_nuclides[0]
 n_evaluations = 100
 
