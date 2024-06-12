ed8b43131625516ff20d3cbea49e897c38b0e1a4
T G <rnt26@cam.ac.uk>
2024-06-12 16:49:41 +0100

    removed print statement

--- Data_handling/uncertainties/propagator_functions.py
+++ Data_handling/uncertainties/propagator_functions.py
@@ -394,7 +394,7 @@ def make_train_sampler(df, validation_nuclides, exclusions, la=0, ua=260):
 	validation_nuclides are explicitly omitted from the training matrix
 	Returns X: X values matrix in shape (nsamples, nfeatures)"""
 
-	print("Forming training matrix...")
+	# print("Forming training matrix...")
 
 	# MT = df['MT']
 	ME = df['ME']
