b2021a5d4d8dfcbeec25ee06b887d4030a19247a
RNTG <rohan.tg05@gmail.com>
2024-07-19 23:28:55 +0100

    params etc

--- Data_handling/uncertainties/propagated_single_exotic_producer.py
+++ Data_handling/uncertainties/propagated_single_exotic_producer.py
@@ -36,9 +36,9 @@ CENDL_nuclides = range_setter(df=CENDL, la=30, ua=210)
 exc = exclusion_func() # 10 sigma with handpicked additions
 
 
-target_nuclides = [[52,118]]
+target_nuclides = [[29,68]]
 target_nuclide = target_nuclides[0]
-n_evaluations = 5
+n_evaluations = 100
 
 
 def diff(target):
