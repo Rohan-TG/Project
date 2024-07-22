93da41ad77716fed5f4b545a3e88a2e32c8d1175
RNTG <rohan.tg05@gmail.com>
2024-07-22 19:30:08 +0100

    new function

--- matrix_functions.py
+++ matrix_functions.py
@@ -5,7 +5,7 @@ from numba.core.errors import NumbaDeprecationWarning
 warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
 import pandas as pd
 import numpy as np
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
 
 def r2_standardiser(library_xs, predicted_xs, gate=0.02):
 	"""Both arguments must be lists.
@@ -30,6 +30,26 @@ def r2_standardiser(library_xs, predicted_xs, gate=0.02):
 
 	return(threshold_gated_predictions, truncated_library_xs, standardised_r2)
 
+
+def rmse_standard(libxs, pxs, gate=0.02):
+	gated_predictions = []
+
+	for p in pxs:
+		if p >= (gate * max(pxs)):
+			gated_predictions.append(p)
+		else:
+			gated_predictions.append(0.0)
+
+	threshold_gated_predictions = []
+	truncated_library_xs = []
+	for i, XS in enumerate(gated_predictions):
+		if XS > 0.0:
+			threshold_gated_predictions.append(XS)
+			truncated_library_xs.append(libxs[i])
+
+	srmse= mean_squared_error(truncated_library_xs, threshold_gated_predictions) **0.5
+
+	return (srmse)
 
 def exclusion_func():
 	exclusionset = [[22, 47], [65, 159], [66, 157], [38, 90], [61, 150],
