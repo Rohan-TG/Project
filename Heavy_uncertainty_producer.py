fad491945795c9050a36e3c83ddd451b5bc9c7a9
RNTG <rohan.tg05@gmail.com>
2024-07-22 19:01:37 +0100

    large updates

--- Heavy_uncertainty_producer.py
+++ Heavy_uncertainty_producer.py
@@ -12,21 +12,15 @@ import time
 from sklearn.metrics import mean_squared_error, r2_score
 import periodictable
 from matrix_functions import dsigma_dE, make_train, make_test,\
-	range_setter, General_plotter
+	range_setter, General_plotter, r2_standardiser
 import scipy.stats
 from datetime import timedelta
 import tqdm
 
 runtime = time.time()
 
-df_test = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
 df = pd.read_csv("ENDFBVIII_MT16_XS_feateng.csv")
 
-df_test = df_test[df_test.Z != 11]
-df = df[df.Z != 11]
-
-df_test.index = range(len(df_test)) # re-label indices
-df.index = range(len(df))
 
 
 al = range_setter(la=210, ua=260, df=df)
@@ -34,12 +28,12 @@ al = range_setter(la=210, ua=260, df=df)
 
 
 
-n_evaluations = 100
+n_evaluations = 10
 datapoint_matrix = []
-target_nuclide = [92,238]
+target_nuclide = [92,235]
 
 for i in tqdm.tqdm(range(n_evaluations)):
-	print(f"\nRun {i+1}/{n_evaluations}")
+	# print(f"\nRun {i+1}/{n_evaluations}")
 
 
 	validation_nuclides = [target_nuclide]
@@ -49,15 +43,15 @@ for i in tqdm.tqdm(range(n_evaluations)):
 		choice = random.choice(al)  # randomly select nuclide from list of all nuclides
 		if choice not in validation_nuclides:
 			validation_nuclides.append(choice)
-	print("\nTest nuclide selection complete")
+	# print("\nTest nuclide selection complete")
 
 	time1 = time.time()
 
 	X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=210, ua=260,) # make training matrix
 
-	X_test, y_test = make_test(validation_nuclides, df=df_test,)
+	X_test, y_test = make_test(validation_nuclides, df=df,)
 
-	print("Data prep done")
+	# print("Data prep done")
 
 	model_seed = random.randint(a=1, b=1000) # seed for subsampling
 
@@ -71,15 +65,19 @@ for i in tqdm.tqdm(range(n_evaluations)):
 
 	model.fit(X_train, y_train)
 
-	print("Training complete")
+	# print("Training complete")
 
-	predictions = model.predict(X_test) # XS predictions
 	predictions_ReLU = []
-	for pred in predictions:
-		if pred >= 0.003:
-			predictions_ReLU.append(pred)
-		else:
-			predictions_ReLU.append(0)
+	for n in validation_nuclides:
+
+		temp_x, temp_y = make_test(nuclides=[n], df=df)
+		initial_predictions = model.predict(temp_x)
+
+		for p in initial_predictions:
+			if p >= (0.02 * max(initial_predictions)):
+				predictions_ReLU.append(p)
+			else:
+				predictions_ReLU.append(0.0)
 
 	predictions = predictions_ReLU
 
@@ -123,9 +121,9 @@ for i in tqdm.tqdm(range(n_evaluations)):
 	exp_true_xs = [y for y in y_test]
 	exp_pred_xs = [xs for xs in predictions]
 
-	print(f"MSE: {mean_squared_error(exp_true_xs, exp_pred_xs, squared=False)}") # MSE
-	print(f"R2: {r2_score(exp_true_xs, exp_pred_xs):0.5f}") # Total R^2 for all predictions in this training campaign
-	print(f'completed in {time.time() - time1:0.1f} s')
+	# print(f"MSE: {mean_squared_error(exp_true_xs, exp_pred_xs, squared=False)}") # MSE
+	# print(f"R2: {r2_score(exp_true_xs, exp_pred_xs):0.5f}") # Total R^2 for all predictions in this training campaign
+	# print(f'completed in {time.time() - time1:0.1f} s')
 
 
 XS_plot = []
@@ -188,12 +186,9 @@ CENDL32.index = range(len(CENDL32))
 CENDL32_energy, CENDL33_XS = General_plotter(df=CENDL32, nuclides=[target_nuclide])
 CENDL_nuclides = range_setter(df=CENDL32, la=210, ua=260)
 mse = mean_squared_error(y_true=true_xs, y_pred=pred_xs)
-r2 = r2_score(true_xs, pred_xs)  # R^2 score for this specific nuclide
-print(f"\n{periodictable.elements[nuc[0]]}-{nuc[1]:0.0f} R2: {r2:0.5f}, MSE: {mse:0.5f}")
-time.sleep(0.8)
 
 pred_endfb_mse = mean_squared_error(pred_xs, true_xs)
-pred_endfb_r2 = r2_score(y_true=true_xs, y_pred=pred_xs)
+d1, d2, pred_endfb_r2 = r2_standardiser(predicted_xs=pred_xs, library_xs=true_xs)
 print(f"Predictions - ENDF/B-VIII R2: {pred_endfb_r2:0.5f} MSE: {pred_endfb_mse:0.6f}")
 
 if target_nuclide in CENDL_nuclides:
@@ -201,7 +196,7 @@ if target_nuclide in CENDL_nuclides:
 	pred_cendl = model.predict(cendl_test)
 
 	pred_cendl_mse = mean_squared_error(pred_cendl, cendl_xs)
-	pred_cendl_r2 = r2_score(y_true=cendl_xs, y_pred=pred_cendl)
+	d1, d2, pred_cendl_r2 = r2_standardiser(predicted_xs=pred_cendl, library_xs=cendl_xs)
 	print(f"Predictions - CENDL3.2 R2: {pred_cendl_r2:0.5f} MSE: {pred_cendl_mse:0.6f}")
 
 if target_nuclide in JENDL_nuclides:
@@ -209,7 +204,7 @@ if target_nuclide in JENDL_nuclides:
 	pred_jendl = model.predict(jendl_test)
 
 	pred_jendl_mse = mean_squared_error(pred_jendl, jendl_xs)
-	pred_jendl_r2 = r2_score(y_true=jendl_xs, y_pred=pred_jendl)
+	d1, d2, pred_jendl_r2 = r2_standardiser(predicted_xs=pred_jendl, library_xs=jendl_xs)
 	print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f}")
 
 if target_nuclide in JEFF_nuclides:
@@ -217,14 +212,14 @@ if target_nuclide in JEFF_nuclides:
 
 	pred_jeff = model.predict(jeff_test)
 
-	pred_jeff_r2 = r2_score(y_true=jeff_xs, y_pred=pred_jeff)
+	d1, d2, pred_jeff_r2 = r2_standardiser(predicted_xs=pred_jeff, library_xs=jeff_xs)
 	print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f}")
 
 if target_nuclide in TENDL_nuclides:
 	tendl_test, tendl_xs = make_test(nuclides=[target_nuclide], df=TENDL)
 	pred_tendl = model.predict(tendl_test)
 
-	pred_tendl_r2 = r2_score(y_true=tendl_xs, y_pred=pred_tendl)
+	d1, d2, pred_tendl_r2 = r2_standardiser(predicted_xs=pred_tendl, library_xs=tendl_xs)
 	print(f"Predictions - TENDL21 R2: {pred_tendl_r2:0.5f}")
 
 
