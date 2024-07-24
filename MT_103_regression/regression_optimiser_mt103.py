3a8ee169b6f46e23922ceb614576d199756694f3
T G <rnt26@cam.ac.uk>
2024-07-24 20:27:47 +0100

    imports

--- MT_103_regression/regression_optimiser_mt103.py
+++ MT_103_regression/regression_optimiser_mt103.py
@@ -1,2 +1,6 @@
 import xgboost as xg
 import pandas as pd
+from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
+from hyperopt.pyll.base import scope
+import hyperopt.early_stop
+from MT_103_functions import make_train, make_test
