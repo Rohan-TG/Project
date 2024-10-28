485cdd4ba589dde8ff7009f44a231d19aaaeaabe
RNTG <rohan.tg05@gmail.com>
2024-10-28 22:53:06 +0000

    run on windows

--- MT_103_regression/mt103_exotic_crossvalidation.py
+++ MT_103_regression/mt103_exotic_crossvalidation.py
@@ -1,157 +1,295 @@
-import random
+import warnings
+
+import periodictable
+from numba.core.errors import NumbaDeprecationWarning
+warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-from MT_103_functions import make_test, make_train, range_setter
-from sklearn.metrics import r2_score
+import random
 import xgboost as xg
 import time
-import datetime
-import matplotlib.pyplot as plt
+import tqdm
+# import shap
+from sklearn.metrics import mean_squared_error, r2_score
+from matrix_functions import range_setter, r2_standardiser, exclusion_func, General_plotter
+from MT_103_functions import make_test, make_train, General_plotter
+import scipy.interpolate
 
-time1 = time.time()
+df = pd.read_csv("ENDFBVIII_100keV_all_uncertainties.csv")
 
-ENDFBVIII = pd.read_csv("ENDFBVIII_MT_103_all_features.csv")
-TENDL21 = pd.read_csv("TENDL-2021_MT_103_all_features.csv")
-JENDL5 = pd.read_csv("JENDL-5_MT_103_all_features.csv")
+gate = 0.0
 
-print("Libraries loaded")
+df.index = range(len(df))
+df = df[(df['Z'] != 6) & (df['A'] != 12)]
+df.index = range(len(df))
+al = range_setter(df=df, la=0, ua=208)
 
-ENDFB_nuclides = range_setter(df=ENDFBVIII, la=30, ua=208)
-TENDL21_nuclides = range_setter(df=TENDL21, la=30, ua=208)
+TENDL = pd.read_csv("TENDL-2021_MT_103_all_features.csv")
+TENDL.index = range(len(TENDL))
+TENDL_nuclides = range_setter(df=TENDL, la=0, ua=208)
 
-all_neutron_deficients = []
-all_proton_deficients = []
+JEFF = pd.read_csv('JEFF-3.3_MT_103_all_features.csv')
+JEFF.index = range(len(JEFF))
+JEFF_nuclides = range_setter(df=JEFF, la=0, ua=208)
 
-exotic_TENDL_nuclides = []
-for tendl_nuclide in TENDL21_nuclides:
-    if tendl_nuclide not in ENDFB_nuclides:
-        exotic_TENDL_nuclides.append(tendl_nuclide)
+JENDL = pd.read_csv('JENDL-5_MT_103_all_features.csv')
+JENDL.index = range(len(JENDL))
+JENDL_nuclides = range_setter(df=JENDL, la=0, ua=208)
 
-nuclides_used = []
+CENDL = pd.read_csv('CENDL-3.2_MT_103_all_features.csv')
+CENDL.index = range(len(CENDL))
+CENDL_nuclides = range_setter(df=CENDL, la=0, ua=208)
 
+exc = exclusion_func()
 
-good_nuclides = []
+num_runs = 2
 
-neutron_excess = 0
-proton_excess = 0
+run_r2 = []
+run_mse = []
 
-mass_difference_with_r2 = []
+nuclide_r2 = []
+nuclide_thresholds = []
+minenergy = 0.1
+maxenergy = 20
 
-print("Forming matrices...")
-
-X_train, y_train = make_train(df=ENDFBVIII, validation_nuclides=[], la=30, ua=210)  # make training matrix
-
-X_test, y_test = make_test(exotic_TENDL_nuclides, df=TENDL21)
-
-
-model_seed = random.randint(a=1,b=1000)
-model = xg.XGBRegressor(n_estimators=900,
-                        learning_rate=0.008,
-                        max_depth=8,
-                        subsample=0.18236,
-                        max_leaves=0,
-                        seed=model_seed, )
-
-print("Training...")
-model.fit(X_train, y_train)
-
-print("Making predictions...")
-predictions = model.predict(X_test)  # XS predictions
-predictions_ReLU = []
-for pred in predictions:
-    if pred >= 0.003:
-        predictions_ReLU.append(pred)
-    else:
-        predictions_ReLU.append(0)
-
-predictions = predictions_ReLU
-
-print("Calculating Differences...")
-# Form arrays for plots below
-XS_plotmatrix = []
-E_plotmatrix = []
-P_plotmatrix = []
-for nuclide in exotic_TENDL_nuclides:
-    dummy_test_XS = []
-    dummy_test_E = []
-    dummy_predictions = []
-    for i, row in enumerate(X_test):
-        if [row[0], row[1]] == nuclide:
-            dummy_test_XS.append(y_test[i])
-            dummy_test_E.append(row[4])  # Energy values are in 5th row
-            dummy_predictions.append(predictions[i])
-
-    XS_plotmatrix.append(dummy_test_XS)
-    E_plotmatrix.append(dummy_test_E)
-    P_plotmatrix.append(dummy_predictions)
-
-r2count = []
-
-for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
-
-    target_exotic_nuclide = exotic_TENDL_nuclides[i]  # exotic nuclide
-
-    r2 = r2_score(y_true=true_xs, y_pred=pred_xs)
-    if r2 > 0.85:
-        r2count.append(r2)
-
-    match_element = []
-    for nuclide in ENDFB_nuclides:
-        if nuclide[0] == target_exotic_nuclide[0]:
-            match_element.append(nuclide)
-    nuc_differences = []
-    for match_nuclide in match_element:
-        difference = match_nuclide[1] - target_exotic_nuclide[1]
-        nuc_differences.append(difference)
-
-    if len(nuc_differences) > 0:
-        if nuc_differences[0] > 0:
-            min_difference = min(nuc_differences)
-            proton_excess += 1
-            all_neutron_deficients.append(r2)
-        elif nuc_differences[0] < 0:
-            min_difference = max(nuc_differences)
-            neutron_excess += 1
-
-            all_proton_deficients.append(r2)
+def diff(target):
+	match_element = []
+	zl = [n[0] for n in al]
+	if target[0] in zl:
+		for nuclide in al:
+			if nuclide[0] == target[0]:
+				match_element.append(nuclide)
+		nuc_differences = []
+		for match_nuclide in match_element:
+			difference = match_nuclide[1] - target[1]
+			nuc_differences.append(difference)
+		if len(nuc_differences) > 0:
+			if nuc_differences[0] > 0:
+				min_difference = min(nuc_differences)
+			elif nuc_differences[0] < 0:
+				min_difference = max(nuc_differences)
+		return (-1 * min_difference)
 
 
-        mass_difference_with_r2.append([r2, min_difference])
 
-    if min_difference > 20:
-        print(target_exotic_nuclide)
+lmdcount = []
+
+hmdcount = []
+
+validation_nuclides = []
+for nuc in TENDL_nuclides:
+	if nuc not in al:
+		validation_nuclides.append(nuc)
+
+for q in tqdm.tqdm(range(num_runs)):
+	nuclides_used = []
+	every_prediction_list = []
+	every_true_value_list = []
+	mdcount80 = 0
+	mdcount90 = 0
+
+	highmd80 = 0
+	highmd90 = 0
+
+	high_md = []
+	tendl_r2s = []
+	low_md = []
+	jeff_r2s = []
 
 
-log_r2_plots = [abs(np.log(abs(i[0]))) for i in mass_difference_with_r2]
-mass_differences = [j[1] for j in mass_difference_with_r2]
+	tendl_rmses = []
+	jendl_r2s = []
 
-plt.plot(mass_differences, log_r2_plots, 'x')
-plt.grid()
-plt.xlabel('Mass differences')
+	outliers = 0
+	outlier_tally = 0
+	tally = 0
+
+	low_masses = []
+
+	nuclide_mse = []
+
+	tally90 = 0
+
+	other = []
+
+	bad_nuclides = []
+
+
+	potential_outliers = []
+
+	benchmark_total_library_evaluations = []
+	benchmark_total_predictions = []
+	# print('Forming data...')
+
+	X_train, y_train = make_train(df=df, validation_nuclides=validation_nuclides, la=0, ua=208,
+							minerg=minenergy, maxerg=maxenergy, mode='both')  # make training matrix
+
+	X_test, y_test = make_test(validation_nuclides, df=TENDL, minerg=minenergy, maxerg=maxenergy)
+
+	modelseed= random.randint(a=1, b=1000)
+	model = xg.XGBRegressor(n_estimators=900,
+							learning_rate=0.01,
+							max_depth=7,
+							subsample=0.2,
+                            reg_lambda=2,
+							max_leaves=0,
+							seed=modelseed,)
+
+
+	model.fit(X_train, y_train)
+
+	predictions = model.predict(X_test)
+
+	XS_plotmatrix = []
+	E_plotmatrix = []
+	P_plotmatrix = []
+	for nuclide in validation_nuclides:
+		dummy_test_XS = []
+		dummy_test_E = []
+		dummy_predictions = []
+		for i, row in enumerate(X_test):
+			if [row[0], row[1]] == nuclide:
+				dummy_test_XS.append(y_test[i])
+				dummy_test_E.append(row[4])  # Energy values are in 5th row
+				dummy_predictions.append(predictions[i])
+
+		XS_plotmatrix.append(dummy_test_XS)
+		E_plotmatrix.append(dummy_test_E)
+		P_plotmatrix.append(dummy_predictions)
+
+	for i, (pred_xs, true_xs, erg) in enumerate(zip(P_plotmatrix, XS_plotmatrix, E_plotmatrix)):
+		nuc = validation_nuclides[i]
+
+		evaluation_r2s = []
+		gated_p = []
+		for p in pred_xs:
+			if p >= (gate * max(pred_xs)):
+				gated_p.append(p)
+			else:
+				gated_p.append(0.0)
+
+		pred_xs = gated_p
+
+
+		nuc_all_library_evaluations = []
+		nuc_all_predictions = []
+
+		jendlerg, jendlxs = General_plotter(df=JENDL, nuclides=[nuc])
+		tendlerg, tendlxs = General_plotter(df=TENDL, nuclides=[nuc])
+
+		interpolation_function = scipy.interpolate.interp1d(tendlerg, y=pred_xs,
+															fill_value='extrapolate')
+
+		pred_tendl_gated, truncated_tendl, tendl_r2 = r2_standardiser(library_xs=true_xs, predicted_xs=pred_xs)
+		tendl_rmse = mean_squared_error(truncated_tendl, pred_tendl_gated)**0.5
+		for libxs, p in zip(truncated_tendl, pred_tendl_gated):
+			nuc_all_library_evaluations.append(libxs)
+			nuc_all_predictions.append(p)
+
+			benchmark_total_library_evaluations.append(libxs)
+			benchmark_total_predictions.append(p)
+
+		evaluation_r2s.append(tendl_r2)
+
+		current_nuclide = nuc
+
+		truncated_library_r2 = []
+
+
+
+		if current_nuclide in JENDL_nuclides:
+			jendlxs_interpolated = interpolation_function(jendlerg)
+
+			predjendlgated, d2, jendl_r2 = r2_standardiser(library_xs=jendlxs, predicted_xs=jendlxs_interpolated)
+			jendl_r2s.append(jendl_r2)
+
+
+			for x, y in zip(d2, predjendlgated):
+				benchmark_total_library_evaluations.append(x)
+				benchmark_total_predictions.append(y)
+
+				nuc_all_library_evaluations.append(x)
+				nuc_all_predictions.append(y)
+
+			evaluation_r2s.append(jendl_r2)
+
+		r2 = r2_score(nuc_all_library_evaluations, nuc_all_predictions)
+
+		rmsescore = mean_squared_error(nuc_all_library_evaluations, nuc_all_predictions) **0.5
+
+		nuclide_r2.append([nuc[0], nuc[1], r2, rmsescore])
+
+		md = diff(nuc)
+		try:
+			if abs(md) <= 3:
+				low_md.append(nuc)
+				if r2 >= 0.8:
+					mdcount80 += 1
+				if r2 >= 0.9:
+					mdcount90 += 1
+
+
+			if abs(md) > 3:
+				high_md.append(nuc)
+				if r2>= 0.8:
+					highmd80 += 1
+				if r2>=90:
+					highmd90 += 1
+		except TypeError:
+			print('No match for ', nuc)
+
+	lmdcount.append(mdcount90)
+	hmdcount.append(highmd90)
+
+zl = [n[0] for n in al]
+print(len(zl))
+
+mins = []
+for c in validation_nuclides:
+	if c[0] in zl:
+		mins.append(c)
+
+print(len(mins))
+
+print('r2 > 80:',mdcount80 , '/', len(low_md))
+print('r2 > 90:',mdcount90 , '/', len(low_md))
+
+print('r2 > 80:', highmd80, '/', len(high_md))
+print('r2 > 90:', highmd90, '/', len(high_md))
+
+agg_n_r2 = range_setter(df=TENDL, la=30,ua=208)
+
+exotics = []
+for ll in agg_n_r2:
+	if ll not in al:
+		exotics.append(ll)
+
+alist = []
+for match in exotics:
+	r2values = []
+	rmsevalues = []
+	for set in nuclide_r2:
+		if [set[0], set[1]] == match:
+			r2values.append(set[2])
+			rmsevalues.append(set[3])
+
+
+	difference = diff([match[0], match[1]])
+
+	alist.append([match[0], match[1], np.mean(r2values), np.mean(rmsevalues), np.std(r2values),
+				  difference])
+
+A_plots = [i[1] for i in alist]
+log_plots = [abs(np.log(abs(i[-3]))) for i in alist]
+
+
+
+plt.figure()
+plt.plot(A_plots, log_plots, 'x')
+# plt.plot(A_plots, r2plot, 'x')
+plt.xlabel("A")
 plt.ylabel("$|\ln(|r^2|)|$")
-plt.title("Exotic nuclide mass differences")
+plt.title("Performance - A")
+plt.grid()
 plt.show()
-
-print(neutron_excess)
-print(proton_excess)
-
-time2 = time.time()
-
-timediff = time2 - time1
-
-print(f"Good prediction count: {len(r2count)}/{len(exotic_TENDL_nuclides)}")
-print(f"Time taken: {str(datetime.timedelta(seconds=timediff))}")
-
-
-tallyp = 0
-for k in all_proton_deficients:
-    if k > 0.85:
-        tallyp += 1
-
-tallyn = 0
-for j in all_neutron_deficients:
-    if j > 0.85:
-        tallyn += 1
-
-print(f"Good neutron-rich: {tallyp}/{len(all_proton_deficients)}")
-print(f"Good proton-rich: {tallyn}/{len(all_neutron_deficients)}")
