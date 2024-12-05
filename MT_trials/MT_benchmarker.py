2ab15b5521a37e31d3b446e086da581a27a5033e
RNTG <rohan.tg05@gmail.com>
2024-12-05 11:52:12 +0000

    modernised

--- MT_trials/MT_benchmarker.py
+++ MT_trials/MT_benchmarker.py
@@ -10,28 +10,28 @@ import xgboost as xg
 from sklearn.metrics import r2_score, mean_squared_error
 import time
 import datetime
-import shap
+import tqdm
 
-TENDL = pd.read_csv("TENDL_2021_MT16_with_ENDFB_MT103_107.csv")
+TENDL = pd.read_csv("TENDL_2021_MT16_XS_features.csv")
 TENDL.index = range(len(TENDL))
 TENDL_nuclides = range_setter(df=TENDL, la=0, ua=210)
 
-JEFF = pd.read_csv('JEFF33_all_features_MT16_103_107.csv')
+JEFF = pd.read_csv('JEFF33_all_features.csv')
 JEFF.index = range(len(JEFF))
 JEFF_nuclides = range_setter(df=JEFF, la=0, ua=210)
 
-JENDL = pd.read_csv('JENDL5_with_MT16_and_ENDFB_MT103_107.csv')
+JENDL = pd.read_csv('JENDL5_arange_all_features.csv')
 JENDL.index = range(len(JENDL))
 JENDL_nuclides = range_setter(df=JENDL, la=0, ua=210)
 
-CENDL = pd.read_csv('CENDL32_MT16_with_ENDFB_MT103_107.csv')
+CENDL = pd.read_csv('CENDL32_all_features.csv')
 CENDL.index = range(len(CENDL))
 CENDL_nuclides = range_setter(df=CENDL, la=0, ua=210)
 
 scriptrun1 = time.time()
 
-df = pd.read_csv('ENDFBVIII_MT16_91_103_107.csv')
-df_test = pd.read_csv('ENDFBVIII_MT16_91_103_107.csv')
+df = pd.read_csv('ENDFBVIII_MT16_MT103_MT107_fund_features.csv')
+df_test = pd.read_csv('ENDFBVIII_MT16_MT103_MT107_fund_features.csv')
 
 df = df[df.Z != 6]
 df_test = df_test[df_test.Z != 6]
@@ -52,7 +52,7 @@ validation_set_size = 10
 num_runs = 10
 run_r2 = []
 tally_95 = []
-for i in range(num_runs+1):
+for i in tqdm.tqdm(range(num_runs)):
 	nuclides_used = []
 	every_prediction_list = []
 	every_true_value_list = []
@@ -76,7 +76,7 @@ for i in range(num_runs+1):
 
 		X_test, y_test = mtmake_test(df= df_test, nuclides=validation_nuclides)
 
-		model = xg.XGBRegressor(n_estimators=900,  # define regressor
+		model = xg.XGBRegressor(n_estimators=950,  # define regressor
 								learning_rate=0.008,
 								max_depth=8,
 								subsample=0.25,
@@ -130,8 +130,14 @@ for i in range(num_runs+1):
 			limited_evaluations = []
 			limited_predictions = []
 
+			jendlerg, jendl_xs = General_plotter(df=JENDL, nuclides=[current_nuclide])
+			cendlerg, cendl_xs = General_plotter(df=CENDL, nuclides=[current_nuclide])
+			jefferg, jeff_xs = General_plotter(df=JEFF, nuclides=[current_nuclide])
+			tendlerg, tendlxs = General_plotter(df=TENDL, nuclides=[current_nuclide])
+			endfberg, endfbxs = General_plotter(df=df, nuclides=[current_nuclide])
+
 			try:
-				pred_endfb_gated, truncated_endfb, endfb_r2 = r2_standardiser(raw_predictions=pred_xs, library_xs=true_xs)
+				pred_endfb_gated, truncated_endfb, endfb_r2 = r2_standardiser(predicted_xs=pred_xs, library_xs=true_xs)
 			except:
 				print(current_nuclide)
 			for libxs, p in zip(truncated_endfb, pred_endfb_gated):
@@ -142,13 +148,15 @@ for i in range(num_runs+1):
 				every_true_value_list.append(libxs)
 			evaluation_r2s.append(endfb_r2)
 
+			interpolation_function = scipy.interpolate.interp1d(erg, y=pred_xs,
+																fill_value='extrapolate')
+
 			if current_nuclide in CENDL_nuclides:
 
-				cendl_test, cendl_xs = mtmake_test(nuclides=[current_nuclide], df=CENDL)
-				pred_cendl = model.predict(cendl_test)
+				pred_cendl = interpolation_function(cendlerg)
 
-				pred_cendl_gated, truncated_cendl, pred_cendl_r2 = r2_standardiser(raw_predictions=pred_cendl,
-																				   library_xs=cendl_xs)
+				pred_cendl_gated, truncated_cendl, pred_cendl_r2 = r2_standardiser(predicted_xs=pred_cendl[1:],
+																				   library_xs=cendl_xs[1:])
 				for libxs, p in zip(truncated_cendl, pred_cendl_gated):
 					all_library_evaluations.append(libxs)
 					all_predictions.append(p)
@@ -163,12 +171,11 @@ for i in range(num_runs+1):
 			# print(f"Predictions - CENDL3.2 R2: {pred_cendl_r2:0.5f} MSE: {pred_cendl_mse:0.6f}")
 
 			if current_nuclide in JENDL_nuclides:
-				jendl_test, jendl_xs = mtmake_test(nuclides=[current_nuclide], df=JENDL)
-				pred_jendl = model.predict(jendl_test)
+				pred_jendl = interpolation_function(jendlerg)
 
-				pred_jendl_mse = mean_squared_error(pred_jendl, jendl_xs)
-				pred_jendl_gated, truncated_jendl, pred_jendl_r2 = r2_standardiser(raw_predictions=pred_jendl,
-																				   library_xs=jendl_xs)
+				pred_jendl_mse = mean_squared_error(pred_jendl[1:], jendl_xs[1:])
+				pred_jendl_gated, truncated_jendl, pred_jendl_r2 = r2_standardiser(predicted_xs=pred_jendl[1:],
+																				   library_xs=jendl_xs[1:])
 				for libxs, p in zip(truncated_jendl, pred_jendl_gated):
 					all_library_evaluations.append(libxs)
 					all_predictions.append(p)
@@ -183,13 +190,11 @@ for i in range(num_runs+1):
 			# print(f"Predictions - JENDL5 R2: {pred_jendl_r2:0.5f} MSE: {pred_jendl_mse:0.6f}")
 
 			if current_nuclide in JEFF_nuclides:
-				jeff_test, jeff_xs = mtmake_test(nuclides=[current_nuclide], df=JEFF)
+				pred_jeff = interpolation_function(jefferg)
 
-				pred_jeff = model.predict(jeff_test)
-
-				pred_jeff_mse = mean_squared_error(pred_jeff, jeff_xs)
-				pred_jeff_gated, truncated_jeff, pred_jeff_r2 = r2_standardiser(raw_predictions=pred_jeff,
-																				library_xs=jeff_xs)
+				pred_jeff_mse = mean_squared_error(pred_jeff[1:], jeff_xs[1:])
+				pred_jeff_gated, truncated_jeff, pred_jeff_r2 = r2_standardiser(predicted_xs=pred_jeff[1:],
+																				library_xs=jeff_xs[1:])
 				for libxs, p in zip(truncated_jeff, pred_jeff_gated):
 					all_library_evaluations.append(libxs)
 					all_predictions.append(p)
@@ -205,12 +210,11 @@ for i in range(num_runs+1):
 			# print(f"Predictions - JEFF3.3 R2: {pred_jeff_r2:0.5f} MSE: {pred_jeff_mse:0.6f}")
 
 			if current_nuclide in TENDL_nuclides:
-				tendl_test, tendl_xs = mtmake_test(nuclides=[current_nuclide], df=TENDL)
-				pred_tendl = model.predict(tendl_test)
+				pred_tendl = interpolation_function(tendlerg)
 
-				pred_tendl_mse = mean_squared_error(pred_tendl, tendl_xs)
-				pred_tendl_gated, truncated_tendl, pred_tendl_r2 = r2_standardiser(raw_predictions=pred_tendl,
-																				   library_xs=tendl_xs)
+				pred_tendl_mse = mean_squared_error(pred_tendl[1:], tendlxs[1:])
+				pred_tendl_gated, truncated_tendl, pred_tendl_r2 = r2_standardiser(predicted_xs=pred_tendl[1:],
+																				   library_xs=tendlxs[1:])
 				for libxs, p in zip(truncated_tendl, pred_tendl_gated):
 					all_library_evaluations.append(libxs)
 					all_predictions.append(p)
