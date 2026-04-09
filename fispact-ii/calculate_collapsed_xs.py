9bf84d305b32fae5e06718b738af1b8220c23f2e
Rohan-TG <rohan.tg05@gmail.com>
2026-04-09 15:37:00 +0100

    working

--- fispact-ii/calculate_collapsed_xs.py
+++ fispact-ii/calculate_collapsed_xs.py
@@ -10,8 +10,12 @@ xs_data = pd.read_csv('Re_185_xs_values.csv')
 xs_values = xs_data['XS'].values
 energy_grid = xs_data['ERG'].values
 
+
+
 fine_grid = np.arange(0, 20.01, 0.01)
 interp_ml_xs = np.interp(fine_grid, energy_grid, xs_values)
+xs_data_interp = pd.DataFrame({'XS': interp_ml_xs, 'ERG': fine_grid})
+
 
 # Interpolate XS values to flux energy group by integrating XS and weighting w.r.t. flux
 energy_bins_data = open('ebins_709', 'r')
@@ -32,7 +36,7 @@ for line in flux_lines[:-2]:
 	split_line = line.split()
 	for item in split_line:
 		flux_list.append(float(item))
-
+flux_list.reverse()
 
 widths = np.diff(energy_list)
 
@@ -48,32 +52,39 @@ plt.show()
 energy_list = np.array(energy_list)
 energy_list = energy_list/1e6
 
-flux_corrected_sigma = []
-for ieb, flux_energy_boundary in enumerate(energy_list):
-	if ieb < len(energy_list)-1:
-		lower_bound = flux_energy_boundary
-		upper_bound = energy_list[ieb+1]
+def correct_sigma(ml_dataframe):
 
-		reduced_ml_sigma = xs_data[(xs_data['ERG'] >= lower_bound) & (xs_data['ERG'] <= upper_bound)]
+	flux_corrected_sigma = []
+	for ieb, flux_energy_boundary in enumerate(energy_list):
+		if ieb < len(energy_list)-1:
+			lower_bound = flux_energy_boundary
+			upper_bound = energy_list[ieb+1]
 
-		sigma_over_E = reduced_ml_sigma['XS'] / reduced_ml_sigma['ERG']
+			reduced_ml_sigma = ml_dataframe[(ml_dataframe['ERG'] >= lower_bound) & (ml_dataframe['ERG'] <= upper_bound)]
 
-		corrected_sigma_numerator = np.trapz(sigma_over_E.values, reduced_ml_sigma['ERG'].values)
+			sigma_over_E = reduced_ml_sigma['XS'] / reduced_ml_sigma['ERG']
 
-		energy_reciprocal = 1 / reduced_ml_sigma['ERG'].values
-		corrected_sigma_d = np.trapz(energy_reciprocal, reduced_ml_sigma['ERG'].values)
+			corrected_sigma_numerator = np.trapz(sigma_over_E.values, reduced_ml_sigma['ERG'].values)
 
-		corrected_sigma = corrected_sigma_numerator / corrected_sigma_d
+			energy_reciprocal = 1 / reduced_ml_sigma['ERG'].values
+			corrected_sigma_d = np.trapz(energy_reciprocal, reduced_ml_sigma['ERG'].values)
 
-		flux_corrected_sigma.append(corrected_sigma)
+			corrected_sigma = corrected_sigma_numerator / corrected_sigma_d
 
-fcs = np.nan_to_num(flux_corrected_sigma)
+			flux_corrected_sigma.append(corrected_sigma)
+
+	fcs = np.nan_to_num(flux_corrected_sigma)
+	return fcs
+
+fcs = correct_sigma(ml_dataframe=xs_data_interp)
 
 plt.figure()
 plt.plot(energy_list[:-1], fcs, 'x-')
 plt.grid()
-plt.xlabel('Energy / eV')
+plt.xlabel('Energy / MeV')
 plt.ylabel('(n,2n) XS')
+plt.title('Interpolated correction')
+plt.xlim(0, 25)
 plt.show()
 
 
@@ -87,3 +98,19 @@ plt.xlabel('Energy / MeV')
 plt.ylabel('(n,2n) XS')
 plt.title('Weighted Re-185 (n,2n) ML cross sections')
 plt.show()
+
+
+
+def collapse(flux_values, corrected_xs):
+
+	numer = np.sum(np.array(corrected_xs) * np.array(flux_values))
+	denom = np.sum(flux_values)
+
+	return numer/denom
+
+collapsed_sigma = collapse(flux_list, fcs)
+
+# tendl_value = 2.070711
+#
+# isomer_fraction = 0.18210701541644395
+# normal_fraction = 0.8178929845835561
