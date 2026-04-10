99f4bcdfe9b4dc7eb46e6822db6429af429546ce
Rohan-TG <rohan.tg05@gmail.com>
2026-04-10 11:26:19 +0100

    asdf

--- fispact-ii/calculate_collapsed_xs.py
+++ fispact-ii/calculate_collapsed_xs.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 
 # Load ML XS
-xs_data = pd.read_csv('Re_185_xs_values.csv')
+xs_filename = 'Pr-141_predictions.csv'
+xs_data = pd.read_csv(xs_filename)
 xs_values = xs_data['XS'].values
 energy_grid = xs_data['ERG'].values
 
@@ -18,33 +19,45 @@ xs_data_interp = pd.DataFrame({'XS': interp_ml_xs, 'ERG': fine_grid})
 
 
 # Interpolate XS values to flux energy group by integrating XS and weighting w.r.t. flux
-energy_bins_data = open('ebins_709', 'r')
-energy_lines = energy_bins_data.readlines()
-energy_list = []
-for line in energy_lines[2:]:
-	linesplit = line.split(',')
-	for item in linesplit:
-		if not item.endswith('\n'):
-			energy_list.append(float(item))
-energy_list.reverse()
 
-fluxes = open('1996exp_7hour_fluxes', 'r')
-flux_lines = fluxes.readlines()
+def fetch_ebins(filename):
+	energy_bins_data = open(f'{filename}', 'r')
+	energy_lines = energy_bins_data.readlines()
+	energy_list = []
+	for line in energy_lines[2:]:
+		linesplit = line.split(',')
+		for item in linesplit:
+			if not item.endswith('\n'):
+				energy_list.append(float(item))
+	energy_list.reverse()
 
-flux_list = []
-for line in flux_lines[:-2]:
-	split_line = line.split()
-	for item in split_line:
-		flux_list.append(float(item))
-flux_list.reverse()
+	return energy_list
+
+energy_list = fetch_ebins('ebins_709')
+
+def fetch_fluxes(flux_filename):
+	fluxes = open(f'{flux_filename}', 'r')
+	flux_lines = fluxes.readlines()
+	flux_list = []
+	for line in flux_lines[:-2]:
+		split_line = line.split()
+		for item in split_line:
+			flux_list.append(float(item))
+	flux_list.reverse()
+
+	return flux_list
+
+flux_list = fetch_fluxes('2000exp_5min_fluxes')
+
 
 widths = np.diff(energy_list)
 
+# Flux plot
 plt.figure()
 plt.bar(energy_list[:-1], flux_list, width=widths)
 plt.grid()
 plt.xlabel('Energy / eV')
-plt.ylabel('Normalised spectrum')
+plt.ylabel('Unnormalised spectrum')
 plt.xscale('log')
 plt.yscale('log')
 plt.show()
@@ -96,7 +109,7 @@ plt.xlim(0, 25)
 plt.grid()
 plt.xlabel('Energy / MeV')
 plt.ylabel('(n,2n) XS')
-plt.title('Weighted Re-185 (n,2n) ML cross sections')
+plt.title('Weighted Pr-141 (n,2n) ML cross sections')
 plt.show()
 
 
@@ -114,3 +127,36 @@ collapsed_sigma = collapse(flux_list, fcs)
 #
 # isomer_fraction = 0.18210701541644395
 # normal_fraction = 0.8178929845835561
+
+norm_flux = np.array(flux_list) / max(flux_list)
+norm_xs = np.array(fcs) / max(fcs)
+
+
+
+# fig, ax1 = plt.subplots()
+#
+# ax1.set_xlabel('Energy')
+# ax1.set_ylabel('Norm flux', color='red')
+# ax1.plot(energy_list[:-1], norm_xs, color='red')
+# ax1.tick_params(axis='y', labelcolor='red')
+#
+# ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+#
+# color = 'tab:blue'
+# ax2.set_ylabel('n,2n', color=color)  # we already handled the x-label with ax1
+# ax2.plot(energy_list[:-1], norm_xs, color=color)
+# ax2.tick_params(axis='y', labelcolor=color)
+#
+# fig.tight_layout()  # otherwise the right y-label is slightly clipped
+# plt.xlim(0, 25)
+# plt.show()
+#
+#
+#
+# plt.figure()
+# plt.bar(energy_list[:-1], norm_flux, width=widths)
+# plt.grid()
+# plt.xlabel('Energy / MeV')
+# plt.xlim(0, 25)
+# plt.yscale('log')
+# plt.show()
